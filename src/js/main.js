dee3840eaa0af5b1c08323d2e61678d420cc0c15
Bryan Nichols <brnicho@rei.com>
2016-02-03 15:36:37 -0800

    Welcome to REI-Cedar

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+require( './affix' );
+require( './alert' );
+require( './button' );
+require( './carousel' );
+require( './collapse' );
+require( './modal' );
+require( './popover' );
+require( './scrollspy' );
+require( './tab' );
+require( './tooltip' );
+require( './transition' );
