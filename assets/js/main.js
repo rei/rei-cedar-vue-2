67db5a25f58e2423942feb8b00fa955250d828c6
Bryan Nichols <brnicho@rei.com>
2016-02-05 14:15:51 -0800

    maybe fixing particles.js?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/assets/js/vendor/particlesjs-config.json', function() {
+particlesJS.load('particles-js', window.sitename + '/assets/js/vendor/particlesjs-config.json', function() {
   console.log('particles.js loaded :)');
 });
