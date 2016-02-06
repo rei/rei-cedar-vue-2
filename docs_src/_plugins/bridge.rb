d55ec56c03b56884451ef3b08e5eacd45569874c
Bryan Nichols <brnicho@rei.com>
2016-02-05 16:08:16 -0800

    Removing Docs from mainline
    This branch removes the docs completely from the mainline branch. The
    rationale behind this is we are leveraging GitHub pages for the
    documentation of this project. Instead of having documentation in two
    places and a convoluted documentation system, we are opting to house the
    docs in an orphan branch -per the recommended procedure on GitHub- and
    all documentation efforts need to be edited then PR approved to be
    merged into the `gh-pages` branch. Indeed this will require a little
    different workflow but in the end, I think this will greatly simplify
    things and it should also prevent wasted effort. An added bonus is it
    keeps the mainline a little cleaner which is kinda nice. Questions and
    comments welcome.

--- docs_src/_plugins/bridge.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-module Bridge
-  class Generator < Jekyll::Generator
-    def generate(site)
-      site.data["configBridge"] = YAML.load_file("./docs_src/_config/configBridge.json")
-    end
-  end
-end
--- docs_src/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/assets/js/vendor/particlesjs-config.json', function() {
-  console.log('particles.js loaded :)');
-});
