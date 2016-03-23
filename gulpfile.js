126ee619503f124a3900cde54cad57e783c8fa03
mhewson <mhewson@gmail.com>
2016-03-23 09:38:32 -0700

    update to add less-plugin-npm-import support

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,6 @@ var csscomb             = require( 'gulp-csscomb' );
 var inject              = require( 'gulp-inject' );
 var uglify              = require( 'gulp-uglify' );
 var streamify           = require( 'gulp-streamify' );
-var cssmin              = require( 'gulp-cssmin' );
 var csslint             = require( 'gulp-csslint' );
 var qunit               = require( 'gulp-qunit' );
 var sourcemaps          = require( 'gulp-sourcemaps' );
@@ -43,7 +42,7 @@ var del                 = require( 'del' );
 var es                  = require( 'event-stream' );
 var runSequence         = require( 'run-sequence' );
 var spawn               = require( 'child_process' ).spawn;
-
+var npmImport        = require('less-plugin-npm-import');
 var pkg                 = require( './package.json' );
 
 
@@ -251,7 +250,7 @@ gulp.task( 'css:clean', () => del( [ path.join( PATHS.DIST, '**/*.css' ) ] ) );
 // Compile the UI framework's Less --> ./dist.
 gulp.task( 'css:build', [ 'css:clean' ], () => {
 
-    const lessc = less( { paths: [ PATHS.LESS ] } ).on( 'error', err => {
+    const lessc = less( { paths: [ PATHS.LESS ], plugins:[new npmImport()]} ).on( 'error', err => {
         console.log( 'There was a problem compiling the LESS files...' );
         throw new Error( err );
     } ); // Break on less compile errors
@@ -503,7 +502,7 @@ gulp.task( 'docs:csscomb-examples', [ 'docs:clean', 'docs:copy-all' ], () =>
 
 gulp.task( 'docs:cssmin-css', [ 'docs:clean', 'docs:copy-all' ], () =>
     gulp.src( PATHS.DOCS_DIST + '/assets/css/src/*.css' )
-        .pipe( cssmin() )
+        .pipe( minifyCss() )
         .pipe( rename( {
             dirname: '/',
             suffix: '.min'
@@ -513,7 +512,7 @@ gulp.task( 'docs:cssmin-css', [ 'docs:clean', 'docs:copy-all' ], () =>
 
 // Build docs CSS and copy into docs src directory
 gulp.task( 'docs:less:compile', [ 'docs:clean', 'docs:copy-all' ], () => {
-    const lessc = less( { paths: [ PATHS.LESS ] } ).on( 'error', err => {
+    const lessc = less( { paths: [ PATHS.LESS ], plugins:[new npmImport()]} ).on( 'error', err => {
         console.log( 'There was a problem compiling the LESS files...' );
         throw new Error( err );
     } ); // Break on less compile errors
