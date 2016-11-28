246deaac3cbc9ec13170f7a0de679aae150a6836
Lance Harper <lancetharper@gmail.com>
2016-11-28 12:26:15 -0800

    Do aria and needed css styles in js, functions for repeated code

--- docs_src/assets/js/src/a11y.js
+++ docs_src/assets/js/src/a11y.js
@@ -4,27 +4,20 @@ $(document).ready(function() {
 });
 
 var keyCodeMap = {
-        48:"0", 49:"1", 50:"2", 51:"3", 52:"4", 53:"5", 54:"6", 55:"7", 56:"8", 57:"9", 59:";",
-        65:"a", 66:"b", 67:"c", 68:"d", 69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l",
-        77:"m", 78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v", 87:"w", 88:"x", 89:"y", 90:"z",
-        96:"0", 97:"1", 98:"2", 99:"3", 100:"4", 101:"5", 102:"6", 103:"7", 104:"8", 105:"9"
+  48:"0", 49:"1", 50:"2", 51:"3", 52:"4", 53:"5", 54:"6", 55:"7", 56:"8", 57:"9", 59:";",
+  65:"a", 66:"b", 67:"c", 68:"d", 69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l",
+  77:"m", 78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v", 87:"w", 88:"x", 89:"y", 90:"z",
+  96:"0", 97:"1", 98:"2", 99:"3", 100:"4", 101:"5", 102:"6", 103:"7", 104:"8", 105:"9"
 }
 
-$.fn.setup_navigation = function(settings) {
+var menuHoverClass = 'js-show-menu';
 
-	settings = jQuery.extend({
-		menuHoverClass: 'show-menu',
-	}, settings);
+$.fn.setup_navigation = function() {
 
 	// Add ARIA role to menubar and menu items
-	// $(this).attr('role', 'menubar').find('li').attr('role', 'menuitem');
+	$(this).attr('role', 'menubar').find('li').attr('role', 'menuitem');
 
 	var top_level_links = $(this).find('> li > a');
-
-	// Added by Terrill: (removed temporarily: doesn't fix the JAWS problem after all)
-	// Add tabindex="0" to all top-level links
-	// Without at least one of these, JAWS doesn't read widget as a menu, despite all the other ARIA
-	//$(top_level_links).attr('tabindex','0');
 
 	// Set tabIndex to -1 so that top_level_links can't receive focus until menu is open
 	$(top_level_links).next('ul')
@@ -33,43 +26,28 @@ $.fn.setup_navigation = function(settings) {
 		.find('a')
 		.attr('tabIndex',-1);
 
-	// We already do this in the HTML
 	// Adding aria-haspopup for appropriate items
-	// $(top_level_links).each(function(){
-	// 	if($(this).next('ul').length > 0)
-	// 		$(this).parent('li').attr('aria-haspopup', 'true');
-	// });
+	$(top_level_links).each(function(){
+		if($(this).next('ul').length > 0)
+			$(this).parent('li').attr('aria-haspopup', 'true');
+	});
 
 	$(top_level_links).hover(function(){
 		$(this).closest('ul')
 			.attr('aria-hidden', 'false')
-			.find('.'+settings.menuHoverClass)
-			.attr('aria-hidden', 'true')
-			.removeClass(settings.menuHoverClass)
-			.find('a')
-			.attr('tabIndex',-1);
+			.find('.'+menuHoverClass)
+			.hideMenu();
 		$(this).next('ul')
-			.attr('aria-hidden', 'false')
-			.addClass(settings.menuHoverClass)
-			.find('a').attr('tabIndex',0);
+			.showMenu();
 	});
 
   $(top_level_links).focus(function(){
 		$(this).closest('ul')
-			// Removed by Terrill
-			// The following was adding aria-hidden="false" to root ul since menu is never hidden
-			// and seemed to be causing flakiness in JAWS (needs more testing)
-			// .attr('aria-hidden', 'false')
-			.find('.'+settings.menuHoverClass)
-			.attr('aria-hidden', 'true')
-			.removeClass(settings.menuHoverClass)
-			.find('a')
-			.attr('tabIndex',-1);
+			.find('.'+menuHoverClass)
+			.hideMenu();
 
     $(this).next('ul')
-			.attr('aria-hidden', 'false')
-			.addClass(settings.menuHoverClass)
-			.find('a').attr('tabIndex',0);
+			.showMenu();
 	});
 
 	// Bind arrow keys for navigation
@@ -86,9 +64,7 @@ $.fn.setup_navigation = function(settings) {
 			e.preventDefault();
 			if($(this).parent('li').find('ul').length > 0) {
 				$(this).parent('li').find('ul')
-					.attr('aria-hidden', 'false')
-					.addClass(settings.menuHoverClass)
-					.find('a').attr('tabIndex',0)
+					.showMenu()
 					.last().focus();
 			}
 		} else if(e.keyCode == 39) {
@@ -103,30 +79,21 @@ $.fn.setup_navigation = function(settings) {
 			e.preventDefault();
 			if($(this).parent('li').find('ul').length > 0) {
 				$(this).parent('li').find('ul')
-					.attr('aria-hidden', 'false')
-					.addClass(settings.menuHoverClass)
-					.find('a').attr('tabIndex',0)
+					.showMenu()
 					.first().focus();
 			}
 		}
-		// This part breaks using the default enter key (13) to activate top level links...maybe add back for space bar? (32)
-		//
-		// else if(e.keyCode == 13 || e.keyCode == 32) {
-		// 	// If submenu is hidden, open it
-		// 	e.preventDefault();
-		// 	$(this).parent('li').find('ul[aria-hidden=true]')
-		// 			.attr('aria-hidden', 'false')
-		// 			.addClass(settings.menuHoverClass)
-		// 			.find('a').attr('tabIndex',0)
-		// 			.first().focus();
-		// }
+		else if(e.keyCode == 32) {
+			// If submenu is hidden, open it
+			e.preventDefault();
+			$(this).parent('li').find('ul[aria-hidden=true]')
+				.showMenu()
+				.first().focus();
+		}
 		else if(e.keyCode == 27) {
 			e.preventDefault();
-			$('.'+settings.menuHoverClass)
-				.attr('aria-hidden', 'true')
-				.removeClass(settings.menuHoverClass)
-				.find('a')
-				.attr('tabIndex',-1);
+			$('.'+menuHoverClass)
+				.hideMenu();
 		} else {
 			$(this).parent('li').find('ul[aria-hidden=false] a').each(function(){
 				if($(this).text().substring(0,1).toLowerCase() == keyCodeMap[e.keyCode]) {
@@ -160,11 +127,8 @@ $.fn.setup_navigation = function(settings) {
 			$(this)
 				.parents('ul').first()
 					.prev('a').focus()
-					.parents('ul').first().find('.'+settings.menuHoverClass)
-					.attr('aria-hidden', 'true')
-					.removeClass(settings.menuHoverClass)
-					.find('a')
-					.attr('tabIndex',-1);
+					.parents('ul').first().find('.'+menuHoverClass)
+					.hideMenu();
 		} else if(e.keyCode == 32) {
 			e.preventDefault();
 			window.location = $(this).attr('href');
@@ -194,17 +158,39 @@ $.fn.setup_navigation = function(settings) {
 	$(this).find('a').last().keydown(function(e){
 		if(e.keyCode == 9) {
 			// If the user tabs out of the navigation hide all menus
-			$('.'+settings.menuHoverClass)
-				.attr('aria-hidden', 'true')
-				.removeClass(settings.menuHoverClass)
-				.find('a')
-					.attr('tabIndex',-1);
+			$('.'+menuHoverClass)
+				.hideMenu();
 		}
 	});
 
-  $(document).click(function(){ $('.'+settings.menuHoverClass).attr('aria-hidden', 'true').removeClass(settings.menuHoverClass).find('a').attr('tabIndex',-1); });
+  $(document).click(function(){
+		$('.'+menuHoverClass)
+			.hideMenu();
+	});
 
 	$(this).click(function(e){
 		e.stopPropagation();
 	});
+}
+
+$.fn.showMenu = function() {
+	$(this)
+		.attr('aria-hidden', 'false')
+		.addClass(menuHoverClass)
+		.css({ left:0, opacity:0.99 })
+		.find('a')
+		.attr('tabIndex',0);
+
+	return $(this)
+}
+
+$.fn.hideMenu = function() {
+	$(this)
+		.attr('aria-hidden', 'true')
+		.removeClass(menuHoverClass)
+		.css({ left: '-999.9rem', opacity: 0 })
+		.find('a')
+		.attr('tabIndex',-1);
+
+	return $(this)
 }
