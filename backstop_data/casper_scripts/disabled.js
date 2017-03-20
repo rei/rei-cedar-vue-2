068dab9a4f3985ed4d739b615b183280733c73be
Lance Harper <lancetharper@gmail.com>
2017-03-20 12:36:27 -0700

    Functioning disabled state casper script for backstop

--- backstop_data/casper_scripts/disabled.js
+++ backstop_data/casper_scripts/disabled.js
@@ -1,6 +1,5 @@
 module.exports = function (casper, scenario, vp) {
-    casper.evaluate(function () {
-        //Your web-app is now loaded. Edit here to simulate user interacions or other state changes.
-        document.querySelector(scenario.selectors[0]).disabled = true;
-    });
+    var el = casper.evaluate(function (s) {
+        document.querySelector(s.selectors[0]).disabled = true;
+    }, scenario);
 };
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div id="app">
+  
     <div data-backstop="test">This is a test</div>
 
-    <div data-backstop="button">
-      <cdr-button>A default button</cdr-button>
-    </div>
-    <div data-backstop="button-red">
-      <cdr-button theme="red">A red button</cdr-button>
-    </div>
-    <div data-backstop="button-blue">
-      <cdr-button theme="blue">A blue button</cdr-button>
-    </div>
+    <cdr-button data-backstop="button">A default button</cdr-button>
+    <cdr-button theme="red" data-backstop="button-red">A red button</cdr-button>
+    <cdr-button theme="blue" data-backstop="button-blue">A blue button</cdr-button>
 
   </div>
 </template>
