ff80bcc6b26f9fef7375359b0c5592e0c18ed3a1
Lance Harper <lancetharper@gmail.com>
2017-03-22 15:49:35 -0700

    Setting up stylelint

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -5,6 +5,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var StyleLintPlugin = require('stylelint-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -30,6 +31,9 @@ module.exports = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true
     }),
-    new FriendlyErrorsPlugin()
+    new StyleLintPlugin({
+      files: ['src/**/main.postcss', 'src/**/*.vue']
+    }),
+    new FriendlyErrorsPlugin(),
   ]
 })
