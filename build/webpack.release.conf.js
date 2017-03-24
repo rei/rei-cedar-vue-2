0aba85e8d5f03bf7e0b7f4d3c75dc5d4be2a61ce
Lance Harper <lancetharper@gmail.com>
2017-03-24 13:30:12 -0700

    Get some updates from vue webpack template

--- build/webpack.release.conf.js
+++ build/webpack.release.conf.js
@@ -1,23 +1,23 @@
-var path = require('path')
-var utils = require('./utils')
-var webpack = require('webpack')
-var config = require('../config')
-var merge = require('webpack-merge')
-var baseWebpackConfig = require('./webpack.base.conf')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var path = require( 'path' )
+var utils = require( './utils' )
+var webpack = require( 'webpack' )
+var config = require( '../config' )
+var merge = require( 'webpack-merge' )
+var baseWebpackConfig = require( './webpack.base.conf' )
+var CopyWebpackPlugin = require( 'copy-webpack-plugin' )
+var ExtractTextPlugin = require( 'extract-text-webpack-plugin' )
+var OptimizeCSSPlugin = require( 'optimize-css-assets-webpack-plugin' )
 
-var env = process.env.NODE_ENV === 'testing'
-    ? require('../config/test.env')
-    : config.release.env
+var env = process.env.NODE_ENV === 'testing' ?
+    require( '../config/test.env' ) :
+    config.release.env
 
-var webpackConfig = merge(baseWebpackConfig, {
+var webpackConfig = merge( baseWebpackConfig, {
     module: {
-        rules: utils.styleLoaders({
+        rules: utils.styleLoaders( {
             sourceMap: config.release.productionSourceMap,
             extract: true
-        })
+        } )
     },
     devtool: config.release.productionSourceMap ? '#source-map' : false,
     output: {
@@ -30,53 +30,55 @@ var webpackConfig = merge(baseWebpackConfig, {
     },
     plugins: [
         // http://vuejs.github.io/vue-loader/en/workflow/production.html
-        new webpack.DefinePlugin({
+        new webpack.DefinePlugin( {
             'process.env': env
-        }),
-        new webpack.optimize.UglifyJsPlugin({
+        } ),
+        new webpack.optimize.UglifyJsPlugin( {
             compress: {
                 warnings: false
             }
-        }),
+        } ),
         // extract css into its own file
-        new ExtractTextPlugin({
+        new ExtractTextPlugin( {
             filename: 'cedar.min.css'
-        }),
+        } ),
         // Compress extracted CSS. We are using this plugin so that possible
         // duplicated CSS from different components can be deduped.
-        new OptimizeCSSPlugin(),
-        // copy custom static assets
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, '../static'),
-                to: config.release.assetsSubDirectory,
-                ignore: ['.*']
+        new OptimizeCSSPlugin( {
+            cssProcessorOptions: {
+                safe: true
             }
-        ])
+        } ),
+        // copy custom static assets
+        new CopyWebpackPlugin( [ {
+            from: path.resolve( __dirname, '../static' ),
+            to: config.release.assetsSubDirectory,
+            ignore: [ '.*' ]
+        } ] )
     ]
-})
+} )
 
-if (config.release.productionGzip) {
-    var CompressionWebpackPlugin = require('compression-webpack-plugin')
+if ( config.release.productionGzip ) {
+    var CompressionWebpackPlugin = require( 'compression-webpack-plugin' )
 
     webpackConfig.plugins.push(
-        new CompressionWebpackPlugin({
+        new CompressionWebpackPlugin( {
             asset: '[path].gz[query]',
             algorithm: 'gzip',
             test: new RegExp(
                 '\\.(' +
-                config.release.productionGzipExtensions.join('|') +
+                config.release.productionGzipExtensions.join( '|' ) +
                 ')$'
             ),
             threshold: 10240,
             minRatio: 0.8
-        })
+        } )
     )
 }
 
-if (config.release.bundleAnalyzerReport) {
-    var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-    webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+if ( config.release.bundleAnalyzerReport ) {
+    var BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin
+    webpackConfig.plugins.push( new BundleAnalyzerPlugin() )
 }
 
 module.exports = webpackConfig
