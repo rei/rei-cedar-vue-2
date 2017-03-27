cb57bf6324c950930dabfcb944ad5350007c047f
Lance Harper <lancetharper@gmail.com>
2017-03-27 12:26:40 -0700

    es6ify main backstop config

--- backstop.js
+++ backstop.js
@@ -1,78 +1,78 @@
 // utilities
-var _ = require('lodash');
-var glob = require('glob');
+const _ = require('lodash');
+const glob = require('glob');
 
 // variables
-var scenarios = [];
-var defs = [];
+const scenarios = [];
+let defs = [];
 
 // get backstop definition files and concat the contents
-var files = glob.sync('./src/**/*.backstop.js');
-files.forEach(function (file) {
-    defs = defs.concat(require(file));
+const files = glob.sync('./src/**/*.backstop.js');
+files.forEach((file) => {
+  defs = defs.concat(require(file));
 });
 
 
 function createSelectorObj(def, selector, onReadyScript) {
-    var rObj = {};
-    _.forOwn(def, function (value, key) {
-        if (key === "selectors") {
-            rObj[key] = [selector];
-        } else if (key === "onReadyScripts") {
-            return;
-        } else {
-            rObj[key] = value;
-        }
-    });
-    if (onReadyScript) {
-        rObj.label = def.label + "_" + onReadyScript;
-        rObj.onReadyScript = onReadyScript;
+  const rObj = {};
+  _.forOwn(def, (value, key) => {
+    if (key === 'selectors') {
+      rObj[key] = [selector];
+    } else if (key === 'onReadyScripts') {
+      
+    } else {
+      rObj[key] = value;
     }
-    return rObj;
+  });
+  if (onReadyScript) {
+    rObj.label = `${def.label }_${onReadyScript}`;
+    rObj.onReadyScript = onReadyScript;
+  }
+  return rObj;
 }
 
-defs.forEach(function (def) {
-    def.url = 'http://localhost:8080/';
-    def.selectors.forEach(function (selector, i) {
-        if (_.has(def, 'onReadyScripts')) {
-            def.onReadyScripts.forEach(function (script) {
-                scenarios.push(createSelectorObj(def, selector, script));
-            });
-        } else {
-            scenarios.push(createSelectorObj(def, selector));
-        }
-    });
+defs.forEach((def) => {
+  def.url = 'http://localhost:8080/';
+  def.selectors.forEach((selector, i) => {
+    if (_.has(def, 'onReadyScripts')) {
+      def.onReadyScripts.forEach((script) => {
+        scenarios.push(createSelectorObj(def, selector, script));
+      });
+    } else {
+      scenarios.push(createSelectorObj(def, selector));
+    }
+  });
 });
 
 module.exports = {
-    "id": "cedar",
-    "viewports": [
-        {
-            "name": "phone",
-            "width": 320,
-            "height": 480
-        },
-        {
-            "name": "tablet_v",
-            "width": 568,
-            "height": 1024
-        },
-        {
-            "name": "tablet_h",
-            "width": 1024,
-            "height": 768
-        }
-    ],
-    "scenarios": scenarios,
-    "paths": {
-        "bitmaps_reference": "backstop_data/bitmaps_reference",
-        "bitmaps_test": "backstop_data/bitmaps_test",
-        "casper_scripts": "backstop_data/casper_scripts",
-        "html_report": "backstop_data/html_report",
-        "ci_report": "backstop_data/ci_report"
+  id: 'cedar',
+  viewports: [
+    {
+      name: 'phone',
+      width: 320,
+      height: 480,
     },
-    "casperFlags": [],
-    "engine": "phantomjs",
-    "report": ["browser"],
-    "debug": false
+    {
+      name: 'tablet_v',
+      width: 568,
+      height: 1024,
+    },
+    {
+      name: 'tablet_h',
+      width: 1024,
+      height: 768,
+    },
+  ],
+  scenarios,
+  paths: {
+    bitmaps_reference: 'backstop_data/bitmaps_reference',
+    bitmaps_test: 'backstop_data/bitmaps_test',
+    casper_scripts: 'backstop_data/casper_scripts',
+    html_report: 'backstop_data/html_report',
+    ci_report: 'backstop_data/ci_report',
+  },
+  casperFlags: [],
+  engine: 'phantomjs',
+  report: ['browser'],
+  debug: false,
 };
