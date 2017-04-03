354035c17f32a220d4fb041c1421e896f5ae4818
Lance Harper <lancetharper@gmail.com>
2017-04-03 15:10:09 -0700

    Small fixes

--- utils/brandai.js
+++ utils/brandai.js
@@ -2,5 +2,5 @@ const fs = require('fs-extra');
 const download = require('download');
 
 download('https://assets.brand.ai/rei/digital-rei-brand/_style-params.css?key=SyQifqFje').then((data) => {
-  fs.writeFileSync('src/css/settings/brandai.css', data);
+  fs.writeFileSync('src/css/settings/brandai.pcss', data);
 });
