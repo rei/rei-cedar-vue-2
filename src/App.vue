9122570e4745d2fd2265caf1c5b4f0fbfa109d59
Lance Harper <lancetharper@gmail.com>
2017-04-13 11:32:34 -0700

    Prototyping headings as a component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app" class="container-fluid">
-    <cdr-heading :level="3">Heading</cdr-heading>
+    <cdr-heading :level="1" class="test">Heading</cdr-heading>
+    <cdr-heading :level="1" :appearance="2">Heading</cdr-heading>
+    <cdr-heading :level="1" :appearance="3">Heading</cdr-heading>
+    <cdr-heading :level="1" :appearance="4">Heading</cdr-heading>
+    <cdr-heading :level="1" :appearance="5">Heading</cdr-heading>
+    <cdr-heading :level="1" :appearance="6">Heading</cdr-heading>
     <test data-backstop="test"></test>
     <cdr-card>
       <cdr-button data-backstop="button">A default button</cdr-button>
--- src/components/headings/Heading.vue
+++ src/components/headings/Heading.vue
@@ -3,15 +3,26 @@ export default {
   name: 'cdr-heading',
   functional: true,
   render(createElement, context) {
+    const contextObj = context;
+    const staticClass = contextObj.data.staticClass;
+    const appearance = contextObj.props.appearance;
+    const appClass = appearance ? `cdr-h${appearance}` : `cdr-h${contextObj.props.level}`;
+    contextObj.data.staticClass = staticClass ? `${staticClass} ${appClass}` : `${appClass}`;
+
     return createElement(
-      `h${context.props.level}`,
-      context.children,
+      `h${contextObj.props.level}`,
+      contextObj.data,
+      contextObj.children,
     );
   },
   props: {
     level: {
       type: Number,
       required: true,
+      validator: value => (value > 0 && value <= 6) || false,
+    },
+    appearance: {
+      type: Number,
       validator: value => (value > 0 && value <= 6) || false,
     },
   },
