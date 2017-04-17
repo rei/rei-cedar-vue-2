66c87d2d882d6c83de6fb4a02d03a418a145e333
mhewson <mhewson@gmail.com>
2017-04-17 09:06:26 -0700

    adding in contrast links and a prototype color class

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,36 @@
     <cdr-heading :level="1" :appearance="4">Heading</cdr-heading>
     <cdr-heading :level="1" :appearance="5">Heading</cdr-heading>
     <cdr-heading :level="1" :appearance="6">Heading</cdr-heading>
+    <cdr-heading :level="2" :appearance="1">Heading 2</cdr-heading>
+    <cdr-heading :level="2" :appearance="2">Heading 2</cdr-heading>
+    <cdr-heading :level="2" :appearance="3">Heading 2</cdr-heading>
+    <cdr-heading :level="2" :appearance="4">Heading 2</cdr-heading>
+    <cdr-heading :level="2" :appearance="5">Heading 2</cdr-heading>
+    <cdr-heading :level="2" :appearance="6">Heading 2</cdr-heading>
     <test data-backstop="test"></test>
-    <cdr-card>
+
+    <cdr-card className="cdr-card">
+      <cdr-anchor data-backstop="cdr-link" hypertextReference="#" className="cdr-link">
+          An example link
+      </cdr-anchor>
+    </cdr-card>
+    <cdr-card className="cdr-card cdr-bg--dark-20">
+      <cdr-anchor data-backstop="cdr-link" hypertextReference="#" className="cdr-link cdr-link--contrast">
+          An example contrast link
+      </cdr-anchor>
+    </cdr-card>
+    <cdr-card className="cdr-card cdr-bg--dark-20">
+      <cdr-anchor data-backstop="cdr-link--standalone" hypertextReference="#" className="cdr-link cdr-link--overlay">
+          An example overlay link
+      </cdr-anchor>
+    </cdr-card>
+    <cdr-card className="cdr-card">
+      <cdr-anchor data-backstop="cdr-link--standalone" hypertextReference="#" className="cdr-link cdr-link--standalone">
+          An example standalone link
+      </cdr-anchor>
+    </cdr-card>
+
+    <cdr-card className="cdr-card">
       <cdr-button data-backstop="button">A default button</cdr-button>
       <cdr-button theme="red" data-backstop="button-red">A red button</cdr-button>
       <cdr-button theme="blue" data-backstop="button-blue">A blue button</cdr-button>
--- src/components/anchors/anchor.play.js
+++ src/components/anchors/anchor.play.js
@@ -6,4 +6,7 @@ play(Anchor)
   .name( 'cdr-anchor' )
   .displayName( 'Anchor' )
   .add( 'cdr-link', '<cdr-anchor hypertextReference="#" className="cdr-link">Sample link</cdr-anchor>' )
-  .add( 'cdr-link--standalone', '<cdr-anchor hypertextReference="#" className="cdr-link cdr-link--standalone">Sample standalone link</cdr-anchor>' );
+  .add( 'cdr-link--contrast', '<cdr-anchor hypertextReference="#" className="cdr-link cdr-link--contrast cdr-bg--dark-20">Sample overlay link</cdr-anchor>' )
+  .add( 'cdr-link--overlay', '<cdr-anchor hypertextReference="#" className="cdr-link cdr-link--overlay cdr-bg--dark-20">Sample overlay link</cdr-anchor>' )
+  .add( 'cdr-link--standalone', '<cdr-anchor hypertextReference="#" className="cdr-link cdr-link--standalone">Sample standalone link</cdr-anchor>' )
+  ;
