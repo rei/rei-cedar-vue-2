b3facbafe4cd1e40539fd9b8ce035422b0bfdbd3
mhewson <mhewson@gmail.com>
2017-04-18 13:05:48 -0700

    update to brand-ai path

--- utils/brandai.js
+++ utils/brandai.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const download = require('download');
 
-download('https://assets.brand.ai/rei/digital-rei-brand/_style-params.css?key=SyQifqFje').then((data) => {
+download('https://assets.brand.ai/REI-DIGITAL-EXPERIENCE-TEAM/digital-rei-brand/_style-params.css?key=SyQifqFje').then((data) => {
   fs.writeFileSync('src/css/settings/brandai.pcss', data);
 });
