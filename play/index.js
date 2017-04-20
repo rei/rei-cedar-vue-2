84e68fdbd4b60fa0acd1ac3b4f208067219ab991
Lance Harper <lancetharper@gmail.com>
2017-04-20 15:06:07 -0700

    Add fonts from cedar 1, demo possible usage with custom properties in headings, load into vue-play

--- play/index.js
+++ play/index.js
@@ -3,3 +3,4 @@ const load = requireContext => requireContext.keys().map(requireContext);
 // load files which end with `.play.js` in `../src/components` folder
 load(require.context('../src/components', true, /.play.js$/));
 load(require.context('../src/css', false, /main.postcss$/));
+load(require.context('../static', false, /fonts.min.css$/));
