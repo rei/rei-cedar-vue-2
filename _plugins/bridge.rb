4e4ee832e96eb3b58edfe16c78e3267736ba0422
Lance Harper <lancetharper@gmail.com>
2017-04-20 10:51:51 -0700

    Update gh-pages to vue-play

--- _plugins/bridge.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-module Bridge
-  class Generator < Jekyll::Generator
-    def generate(site)
-      site.data["configBridge"] = YAML.load_file("_config/configBridge.json")
-    end
-  end
-end
