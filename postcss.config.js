cf58a0ddf756bd06cece931272a49e586aa931bb
Lance Harper <lancetharper@gmail.com>
2017-05-04 09:28:28 -0700

    Add postcss plugin "pxtorem"

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,8 @@ module.exports = {
     require('postcss-import')({ /* ...options */ }),
     require('postcss-cssnext')({ /* ...options */ }),
     require('postcss-responsive-type')(),
+    require('postcss-pxtorem')({
+      rootValue: 10,
+    }),
   ],
 };
