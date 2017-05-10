84f4ae2641a098590e31dafc06ce29b7440869d4
Lance Harper <lancetharper@gmail.com>
2017-05-10 16:06:29 -0700

    Prototype component to inline svg

---
+++ src/components/icons/Icon.vue
@@ -0,0 +1,20 @@
+
+<script>
+import iconComps from './iconComps';
+
+export default {
+  name: 'cdr-icon',
+  functional: true,
+  render(h, context) {
+    const data = context.data;
+    const children = context.children;
+    const props = context.props;
+
+    const staticClass = data.staticClass ? `cdr-icon ${data.staticClass}` : 'cdr-icon';
+
+    data.staticClass = staticClass;
+    return h(iconComps[props.name], data, children);
+  },
+};
+</script>
+
---
+++ src/components/icons/iconComps.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+const addSvg = require('../../assets/icons/icon-rei-add.svg');
+
+const add = Vue.component('icon-rei-add', {
+  template: addSvg,
+});
+
+export default {
+  add,
+};
