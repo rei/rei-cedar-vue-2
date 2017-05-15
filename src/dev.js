fd86b19fda7b51ea11857da28db25c69a3b31575
Lance Harper <lancetharper@gmail.com>
2017-05-15 15:42:53 -0700

    Add vue router, set up routes, upgrade deps

--- src/dev.js
+++ src/dev.js
@@ -1,16 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import 'svgxuse';
 
 import './css/main.postcss';
 import App from './App';
 
-Vue.config.productionTip = false;
+// test pages
+import eaHome from './expertAdvice/Home';
 
+// routing
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: App },
+  { path: '/ea', component: eaHome },
+];
+
+const router = new VueRouter({ routes });
+
+Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App },
-});
+  router,
+}).$mount('#app');
---
+++ src/expertAdvice/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Expert Advice!</h1>
+    <cdr-button>Hi</cdr-button>
+  </div>
+</template>
+
+<script>
+import Components from '../components/_index';
+
+export default {
+  name: 'ea-home',
+  components: Components,
+};
+</script>
