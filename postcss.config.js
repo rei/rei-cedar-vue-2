6520d47d98862dbf8624b93daa3da73081876267
Lance Harper <lancetharper@gmail.com>
2017-05-17 14:09:07 -0700

    Upgrade some dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  plugins: [
-    require('postcss-import')({ /* ...options */ }),
-    require('postcss-cssnext')({ /* ...options */ }),
-    require('postcss-responsive-type')(),
-    require('postcss-pxtorem')({
+  parser: 'postcss-scss',
+  plugins: {
+    'postcss-import': {},
+    'postcss-cssnext': {},
+    // 'postcss-comment': {},
+    'postcss-pxtorem': {
       rootValue: 10,
-    }),
-  ],
+    },
+  },
+  // plugins: [
+  //   require('postcss-import')({ /* ...options */ }),
+  //   require('postcss-cssnext')({ /* ...options */ }),
+  //   require('postcss-pxtorem')({
+  //     rootValue: 10,
+  //   }),
+  // ],
 };
