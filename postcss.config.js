b5db5d20c2e8d3a9f48731cfbc70d6b973195df7
Lance Harper <lancetharper@gmail.com>
2017-05-17 14:57:04 -0700

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-  parser: 'postcss-scss',
+  parser: 'postcss-scss', // for stripping comments from brand.ai css
   plugins: {
     'postcss-import': {},
     'postcss-cssnext': {},
-    // 'postcss-comment': {},
     'postcss-pxtorem': {
       rootValue: 10,
     },
   },
-  // plugins: [
-  //   require('postcss-import')({ /* ...options */ }),
-  //   require('postcss-cssnext')({ /* ...options */ }),
-  //   require('postcss-pxtorem')({
-  //     rootValue: 10,
-  //   }),
-  // ],
 };
