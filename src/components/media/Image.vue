916aadc1d5b2adc76dde6f8d3fce104bde1e3156
Lance Harper <lancetharper@gmail.com>
2017-05-19 10:56:13 -0700

    Setup space for image testing

---
+++ src/components/media/Image.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cdr-media-frame">
+    <img class="cdr-media-frame__image" :src="src" :alt="alt">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cdr-img',
+  props: {
+    src: String,
+    alt: String,
+  },
+};
+</script>
---
+++ src/examples/media/Images.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <cdr-row :cols="4">
+      <cdr-col>
+        <cdr-img src="http://placehold.it/350x150"></cdr-img>
+      </cdr-col>
+      <cdr-col>
+        <cdr-img src="http://placehold.it/350x150"></cdr-img>
+      </cdr-col>
+      <cdr-col>
+        <cdr-img src="http://placehold.it/350x150"></cdr-img>
+      </cdr-col>
+      <cdr-col>
+        <cdr-img src="http://placehold.it/350x150"></cdr-img>
+      </cdr-col>
+    </cdr-row>
+    <cdr-img src="http://placehold.it/350x150"></cdr-img>
+  </div>
+</template>
+
+<script>
+  import Components from '../../components/_index';
+
+  export default {
+    name: 'images',
+    components: Components,
+  };
+</script>
