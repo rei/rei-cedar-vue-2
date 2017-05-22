f7aea8af9264852fdbf2ef19064fda5b0a3709e8
Lance Harper <lancetharper@gmail.com>
2017-05-22 15:22:49 -0700

    Fix tenon a11y issues

--- src/examples/media/demos/Ratios.vue
+++ src/examples/media/demos/Ratios.vue
@@ -3,55 +3,55 @@
         <cdr-col>
           <div>
             <p>Square</p>
-            <cdr-img ratio="square" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="square" alt="ratio square" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>1-2</p>
-            <cdr-img ratio="1-2" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="1-2" alt="ratio 1-2" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>2-3</p>
-            <cdr-img ratio="2-3" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="2-3" alt="ratio 2-3" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>3-4</p>
-            <cdr-img ratio="3-4" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="3-4" alt="ratio 3-4" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>9-16</p>
-            <cdr-img ratio="9-16" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="9-16" alt="ratio 9-16" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>2-1</p>
-            <cdr-img ratio="2-1" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="2-1" alt="ratio 2-1" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>3-2</p>
-            <cdr-img ratio="3-2" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="3-2" alt="ratio 3-2" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>4-3</p>
-            <cdr-img ratio="4-3" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="4-3" alt="ratio 4-3" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
         <cdr-col>
           <div>
             <p>16-9</p>
-            <cdr-img ratio="16-9" alt="cdr-img example" src="http://placehold.it/300x100"></cdr-img>
+            <cdr-img ratio="16-9" alt="ratio 16-9" src="http://placehold.it/300x100"></cdr-img>
           </div>
         </cdr-col>
       </cdr-row>
