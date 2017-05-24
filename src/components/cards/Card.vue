cad4cd497ddf603b8b0b088b19990ec015cd5ca2
mhewson <mhewson@gmail.com>
2017-05-24 09:51:16 -0700

    first pass at card styles from cedar1

--- src/components/cards/Card.vue
+++ src/components/cards/Card.vue
@@ -1,13 +1,94 @@
 <template>
-  <section class="cdr-card">
-    <slot></slot>
+  <section :class="[buildClass]" v-if="!href">
+    <!-- Card Image Slot-->
+    <slot name="img" ref="img">
+        <img v-if="img" :src="img" :alt="imgAlt" class="cdr-card__img">
+    </slot>
+    <!--Show custom block when no-block prop is set-->
+    <template v-if="noBlock">
+        <slot></slot>
+    </template>
+    <!-- Card Standard Blocks-->
+    <div :class="blockClass" v-else>
+        <slot></slot>
+    </div>
   </section>
+
+  <a :class="[buildClass]" :href="href" :target="target" :rel="newRel" v-else>
+    <!-- Card Image Slot-->
+    <slot name="img" ref="img">
+        <img v-if="img" :src="img" :alt="imgAlt" class="cdr-card__img">
+    </slot>
+    <!--Show custom block when no-block prop is set-->
+    <template v-if="noBlock">
+        <slot></slot>
+    </template>
+    <!-- Card Standard Blocks-->
+    <div :class="blockClass" v-else>
+        <slot></slot>
+    </div>
+  </a>
 </template>
 
 <script>
   export default {
     name: 'cdr-card',
-};
+    props: {
+      href: String,
+      target: String,
+      rel: String,
+      theme: String,
+      modifier: {
+        required: false,
+        default: () => [],
+      },
+      noBlock: {
+        type: Boolean,
+        default: false,
+      },
+      // Image
+      img: {
+        type: String,
+        default: null,
+      },
+      imgAlt: {
+        type: String,
+        default: null,
+      },
+      overlay: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    computed: {
+      buildClass() {
+        const baseClass = 'cdr-card';
+        let final = '';
+
+        if (this.theme) {
+          final += `${this[this.theme][baseClass]} `;
+
+          this.modifier.forEach((mod) => {
+            final += `${this[this.theme][`${baseClass}--${mod}`]} `;
+          });
+        } else {
+          final += `${baseClass} `;
+
+          this.modifier.forEach((mod) => {
+            final += `${baseClass}--${mod} `;
+          });
+        }
+
+        return final;
+      },
+      blockClass() {
+        return [
+          'cdr-card__block',
+          this.overlay ? 'cdr-card__img--overlay' : null,
+        ];
+      },
+    },
+  };
 </script>
 
 <style>
--- src/expertAdvice/Home.vue
+++ src/expertAdvice/Home.vue
@@ -1,66 +1,158 @@
 <template>
-  <div class="container-fluid">
-  <h1>Expert Advice!</h1>
-    <cdr-row :cols="1">
-      <cdr-col :span="10" :offset-left="1">
-         <p>Your go-to source for outdoor knowledge you can trust. All the information you need to learn new skills, choose and maintain gear, prep for your trips and have amazing adventures.</p>
+  <div class="container">
+  <cdr-row :cols="1" :justify="'center'">
+    <cdr-col :span="10" :offset-left="1">
+      <p>Your go-to source for outdoor knowledge you can trust. All the information you need to learn new skills, choose and maintain gear, prep for your trips and have amazing adventures.</p>
       </cdr-col>
-      <cdr-col :span="10" :offset-left="1">
-        <cdr-list :modifier="['unstyled']">
-          <li>Camping</li>
-          <li>Hiking</li>
-          <li>Cycling</li>
-          <li>Climbing</li>
-          <li>Snowsports</li>
-          <li>Paddling</li>
-          <li>Fitness</li>
-          <li>Travel</li>
-          <li>Clothing</li>
-          <li>Outdoor Basics</li>
-          <li>Family</li>
-        </cdr-list>
+       <cdr-col :span="10" :offset-left="1" :isRow="true" :cols="2" :colsMd="6" justifyMd="center" type="list">
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-camping"></cdr-icon>
+            Camping
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-hiking"></cdr-icon>
+            Hiking
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-cycling"></cdr-icon>
+            Cycling
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-climbing"></cdr-icon>
+            Climbing
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-snowsports"></cdr-icon>
+            Snowsports
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-paddling"></cdr-icon>
+            Paddling
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+           <cdr-icon url="/static/ea-icons.svg#ea-fitness"></cdr-icon>
+           Fitness
+           </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-travel"></cdr-icon>
+            Travel
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-clothing"></cdr-icon>
+            Clothing
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-outdoor-basics"></cdr-icon>
+            Outdoor Basics
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <cdr-icon url="/static/ea-icons.svg#ea-family"></cdr-icon>
+            Family
+          </div>
+        </cdr-col>
+    </cdr-col>
+    <cdr-col>
+      <cdr-heading :level="2"  :modifier="['section-title']">
+        Editor's Picks
+      </cdr-heading>
+    </cdr-col>
+    <cdr-col :span="12" :span-md="12" :isRow="true">
+      <cdr-col :span="12" :span-md="6">
+        <cdr-card>
+          <cdr-heading :level="3" :modifier="['title']">
+          Glacier National Park Backpacking Packing List
+          </cdr-heading>
+        </cdr-card>
       </cdr-col>
-      <cdr-col :span="12" :isRow="true">
-        <cdr-col :span="12" :span-md="12" :isRow="true">
-          <cdr-col :span="12" :span-md="6">
-            <cdr-card>
-             <cdr-heading :level="2" :modifier="['title']">Glacier National Park Backpacking Packing List</cdr-heading>
-            </cdr-card>
-          </cdr-col>
-          <cdr-col :span="12" :span-md="6" :isRow="true" :cols="1" :cols-md="2">
-           <cdr-col>
-            <cdr-card>
-              <cdr-heading :level="2"  :modifier="['section-title']">Backpacking Tents: How to Choose</cdr-heading>
-            </cdr-card>
-            </cdr-col>
-              <cdr-col>
-                <cdr-card>
-              <cdr-heading :level="2"  :modifier="['section-title']">Kids and Camping</cdr-heading>
-            </cdr-card>
-              </cdr-col>
-          </cdr-col>
+      <cdr-col :span="12" :span-md="6" :isRow="true" :cols="1" :cols-md="2">
+        <cdr-col>
+          <cdr-card>
+            <img src=""
+            <cdr-heading :level="3"  :modifier="['section-title']">Backpacking Tents: How to Choose</cdr-heading>
+          </cdr-card>
+        </cdr-col>
+        <cdr-col>
+          <cdr-card>
+            <cdr-heading :level="3"  :modifier="['section-title']">Kids and Camping</cdr-heading>
+          </cdr-card>
         </cdr-col>
       </cdr-col>
-      <cdr-col :span="12" :isRow="true">
-        <cdr-col :span="12" :span-md="12" :isRow="true">
-          <cdr-col :span="12" :span-md="6" :isRow="true" :cols="1" :cols-md="2">
-           <cdr-col>
-            <cdr-card>
-              <cdr-heading :level="2"  :modifier="['section-title']">Food Storage and Handling for Campers and Backpackers</cdr-heading>
+    </cdr-col>
+    <cdr-col :span="12" :isRow="true">
+      <cdr-col :span="12" :span-md="12" :isRow="true">
+        <cdr-col :span="12" :span-md="6" :isRow="true" :cols="1" :cols-md="2">
+          <cdr-col>
+            <cdr-card glaze="dark-20">
+              <cdr-heading :level="3"  :modifier="['section-title']">
+              Food Storage and Handling for Campers and   Backpackers
+              </cdr-heading>
             </cdr-card>
             </cdr-col>
               <cdr-col>
-                <cdr-card>
-              <cdr-heading :level="2"  :modifier="['section-title']">Yellowstone National Park Kayaking and Canoeing Gear List</cdr-heading>
+                <cdr-card href="https://rei.com">
+              <cdr-heading :level="3"  :modifier="['section-title']">Yellowstone National Park Kayaking and Canoeing Gear List</cdr-heading>
             </cdr-card>
               </cdr-col>
           </cdr-col>
            <cdr-col :span="12" :span-md="6">
-            <cdr-card>
-             <cdr-heading :level="2" :modifier="['title']">Zion National Park Visitor Guide</cdr-heading>
+            <cdr-card :modifier="['dark-10']">
+             <cdr-heading :level="3" :modifier="['title']">Zion National Park Visitor Guide</cdr-heading>
             </cdr-card>
           </cdr-col>
         </cdr-col>
+      </cdr-col>
+      <cdr-col>
+      <div>
+      <!--make into heading - sub-heading combo-->
+      <cdr-heading :level="2"  :modifier="['title']">
+          Get hands on guidance - take a class
+      </cdr-heading>
+       <cdr-heading :level="3"  :modifier="['section-title']">
+         Popular Classes &amp; Events near Seattle, Wa
+      </cdr-heading>
+      </div>
+      </cdr-col>
+      <cdr-col :span="4">
+        <cdr-card>
+          <cdr-heading :level="3" :modifier="['title']">
+            Zion National Park Visitor Guide
+          </cdr-heading>
+        </cdr-card>
+      </cdr-col>
+      <cdr-col :span="4">
+        <cdr-card>
+          <cdr-heading :level="3" :modifier="['title']">
+            Zion National Park Visitor Guide
+          </cdr-heading>
+        </cdr-card>
+      </cdr-col>
+      <cdr-col :span="4">
+        <cdr-card>
+          <cdr-heading :level="3" :modifier="['title']">
+            Zion National Park Visitor Guide
+          </cdr-heading>
+        </cdr-card>
       </cdr-col>
     </cdr-row>
 
