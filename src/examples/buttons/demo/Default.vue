1643eb90922645a3914ae4c2921794987570f1f5
Lance Harper <lancetharper@gmail.com>
2017-06-16 15:25:33 -0700

    Fix button demos

--- src/examples/buttons/demo/Default.vue
+++ src/examples/buttons/demo/Default.vue
@@ -1,8 +1,8 @@
 <template>
   <cdr-button-group class="button-example">
-    <cdr-button data-backstop="cdr-button" type="submit" :handle-click="log">Default with click</cdr-button>
-    <cdr-button :type="'submit'" :modifier="['small']">Default small submit</cdr-button>
-    <cdr-button :type="'reset'" :modifier="['extra-small']">Default extra small reset</cdr-button>
+    <cdr-button data-backstop="cdr-button" type="submit" :on-click="log">Default with click</cdr-button>
+    <cdr-button type="submit" modifier="small">Default small submit</cdr-button>
+    <cdr-button type="reset" modifier="extra-small">Default extra small reset</cdr-button>
   </cdr-button-group>
 </template>
 
@@ -14,7 +14,7 @@ export default {
   components: Components,
   methods: {
     log() {
-      this.$log('button clicked');
+      console.log('clicked'); // eslint-disable-line
     },
   },
 };
--- src/examples/buttons/demo/Link.vue
+++ src/examples/buttons/demo/Link.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h3>Buttons with link styles</h3>
-    <p><cdr-button :modifier="['link']">Button as link</cdr-button></p>
-    <p><cdr-button data-backstop="cdr-button--link-standalone" :modifier="['link', 'standalone']">Button as link--standalone</cdr-button></p>
+    <p><cdr-button modifier="link">Button as link</cdr-button></p>
+    <p><cdr-button data-backstop="cdr-button--link-standalone" modifier="link standalone">Button as link--standalone</cdr-button></p>
   </div>
 </template>
 
