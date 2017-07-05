47ced59534ccd0ca73015ce8b0839522c819df37
mhewson <mhewson@gmail.com>
2017-07-05 11:02:23 -0700

    a couple more tests for media object

--- src/components/media/__tests__/cdrMediaObject.spec.js
+++ src/components/media/__tests__/cdrMediaObject.spec.js
@@ -25,7 +25,20 @@ describe('cdrMediaObject.vue', () => {
         mediaFigure: 'http://via.placeholder.com/350x150',
       }
     });
-    expect(wrapper.contains('img')).to.equal(true);
+    const img = wrapper.find('img')[0];
+    expect(img.is('img')).to.equal(true);
+    expect(img.hasClass('cdr-media-object__figure')).to.equal(true);
+  });
+  it('allows a heading lvl to be selected', () => {
+    const wrapper = mount(cdrMediaObject, {
+      propsData: {
+        mediaFigure: 'http://via.placeholder.com/350x150',
+        mediaTitle: 'test title',
+        level : '6',
+      }
+    });
+    const heading = wrapper.find('h6')[0];
+    expect(heading.is('h6')).to.equal(true);
   });
   it('aligns an image to the top', () => {
     const wrapper = mount(cdrMediaObject, {
