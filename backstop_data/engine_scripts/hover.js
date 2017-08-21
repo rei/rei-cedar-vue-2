c15fd5cb5495a98716954d3bc9f16fae045deab5
Lance Harper <lancetharper@gmail.com>
2017-08-21 13:21:53 -0700

    Update backstop, fix script for hover for use in chrome

--- backstop_data/engine_scripts/hover.js
+++ backstop_data/engine_scripts/hover.js
@@ -1,8 +1,7 @@
 module.exports = function (chromy, scenario, vp) {
-  console.log('>>>>>>');
-  // console.log(chromy.rect(scenario.selectors[0]));
-
-  // chromy.select(scenario.selectors[0]);
-
-  // .mouse.move(scenario.selectors[0]);
+  chromy
+    .rect(scenario.selectors[0])
+    .result((rect) => {
+      chromy.mouseMoved(rect.left, rect.top);
+    });
 };
