d7f5c1922152a5e29dec94246148503f7c32c1f7
Lance Harper <lancetharper@gmail.com>
2017-08-22 12:20:37 -0700

    Update backstop, fix selector

--- src/components/rating/cdrRating.backstop.js
+++ src/components/rating/cdrRating.backstop.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     label: 'Rating',
     selectors: [
-      "[data-backstop='ratings']",
+      '[data-backstop="ratings"]',
     ],
     delay: 0,
     misMatchThreshold: 0.1,
