87fd0ec797af907ce93ac9afce46929a5d80f103
Lance Harper <lancetharper@gmail.com>
2017-09-18 11:28:23 -0700

    Make responsive button opt-in, update examples

--- src/components/anchor/examples/demo/Button.vue
+++ src/components/anchor/examples/demo/Button.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="anchor-example">
-    <cdr-a data-backstop="cdr-link--button" href="https://www.rei.com/" modifier="button">Link as button</cdr-a>
+    <p>
+      <cdr-a data-backstop="cdr-link--button" href="https://www.rei.com/" modifier="button responsive">Link as button</cdr-a>
+    </p>
+    <p>
+      <cdr-a data-backstop="cdr-link--button" href="https://www.rei.com/" modifier="button">Link as button (not responsive)</cdr-a>
+    </p>
   </div>
 </template>
 
