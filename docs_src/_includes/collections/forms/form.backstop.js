0b27d5f139af5af73de9d77d5f86ff9ca4aab0f1
mhewson <mhewson@gmail.com>
2017-09-20 15:13:16 -0700

    documentation updates to form, validation updates to form elements in col structures

--- docs_src/_includes/collections/forms/form.backstop.js
+++ docs_src/_includes/collections/forms/form.backstop.js
@@ -5,18 +5,7 @@ module.exports = [
     "selectors": [
       "[data-example-id='basic-forms']",
       "[data-example-id='simple-form-inline']",
-      "[data-example-id='form-inline-with-input-group']",
-      "[data-example-id='simple-horizontal-form']",
-      "[data-example-id='disabled-fieldset']",
-      "[data-example-id='form-control-height-sizes']",
-      "[data-example-id='form-group-height-sizes']",
-      "[data-example-id='form-control-column-sizing']",
-      "[data-example-id='simple-help-text']",
-      "[data-example-id='form-validation-states']",
-      "[data-example-id='form-validation-states-with-icons']",
-      "[data-example-id='horizontal-form-validation-state-with-icon']",
-      "[data-example-id='inline-form-validation-state-with-icon']",
-      "[data-example-id='form-validation-state-with-icon-without-label']"
+      "[data-example-id='validation-examples']"
     ],
     "readyEvent": null,
     "delay": 500,
