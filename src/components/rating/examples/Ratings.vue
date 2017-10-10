be491c816a81b3bbf13ec226cba179818614cf54
Lance Harper <lharper@izeni.com>
2017-10-10 13:04:01 -0700

    Icons without extra whitespace

--- src/components/rating/examples/Ratings.vue
+++ src/components/rating/examples/Ratings.vue
@@ -2,8 +2,8 @@
   <div>
     <h2>Ratings</h2>
   
-    <cdr-rating rating="0" count="10" modifier="large"></cdr-rating>
-    <cdr-rating rating="3" count="100" modifier="large" compact></cdr-rating>
+    <cdr-rating rating="0.25" count="10" modifier="large"></cdr-rating>
+    <cdr-rating rating="3.75" count="100" modifier="large" compact></cdr-rating>
 
     <cdr-rating rating="1.2" count="77" modifier="medium"></cdr-rating>
     <cdr-rating rating="4.8" count="4561" modifier="medium" compact></cdr-rating>
