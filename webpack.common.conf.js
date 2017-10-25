9fa40e98d914e90aee329f746253a74fe0881324
Allen Taylor <allen.taylor@paviasystems.com>
2017-10-25 11:27:59 -0700

    Updating webpack configuration

--- webpack.common.conf.js
+++ webpack.common.conf.js
@@ -13,7 +13,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       vue$: 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': './src',
     },
     modules: [
       path.join(__dirname, '../node_modules')
