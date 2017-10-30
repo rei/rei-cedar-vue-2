dd38442a2494b563728ad30377007d06714f4a9d
Allen Taylor <allen.taylor@paviasystems.com>
2017-10-30 16:19:21 -0700

    Adding js file for dev build of cedar

---
+++ src/dev.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import 'svgxuse';
+
+import './css/main.postcss';
+import App from './App';
+
+// test pages
+import eaHome from './expertAdvice/Home';
+import goResults from './getOutside/Results';
+import goDetails from './getOutside/Details';
+import goLanding from './getOutside/Landing';
+
+Vue.config.devtools = false;
+// routing
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: App },
+  { path: '/ea', component: eaHome },
+  { path: '/go-results', component: goResults },
+  { path: '/go-details', component: goDetails },
+  { path: '/go-landing', component: goLanding },
+];
+
+const router = new VueRouter({ routes });
+
+Vue.config.productionTip = false;
+/* eslint-disable no-new */
+new Vue({
+  router,
+}).$mount('#main');
