ec5dfe1ddc0e7e169c55ed9227bbd1021c7b4f62
Allen Taylor <allen.taylor@paviasystems.com>
2017-11-06 16:41:12 -0800

    Adding kitchen sink base component package

---
+++ src/bundles/baseComponents/_index.js
@@ -0,0 +1,31 @@
+import cdrA from '@rei-co-op/cedar-a';
+import cdrButton from '@rei-co-op/cedar-button';
+import cdrCard from '@rei-co-op/cedar-card';
+import cdrCheckbox from '@rei-co-op/cedar-checkbox';
+import cdrCol from '@rei-co-op/cedar-column';
+import cdrHeading from '@rei-co-op/cedar-heading';
+import cdrIcon from '@rei-co-op/cedar-icon';
+import cdrImg from '@rei-co-op/cedar-image';
+import cdrInput from '@rei-co-op/cedar-input';
+import cdrList from '@rei-co-op/cedar-list';
+import cdrRadio from '@rei-co-op/cedar-radio';
+import cdrRating from '@rei-co-op/cedar-rating';
+import cdrRow from '@rei-co-op/cedar-row';
+import cdrSelect from '@rei-co-op/cedar-select';
+
+export default {
+  cdrA,
+  cdrButton,
+  cdrCard,
+  cdrCheckbox,
+  cdrCol,
+  cdrHeading,
+  cdrIcon,
+  cdrImg,
+  cdrInput,
+  cdrList,
+  cdrRadio,
+  cdrRating,
+  cdrRow,
+  cdrSelect,
+};
