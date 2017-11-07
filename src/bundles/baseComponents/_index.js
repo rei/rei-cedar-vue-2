96d87995f1298dbea6c53bb9075dee423aade53f
Allen Taylor <allen.taylor@paviasystems.com>
2017-11-07 12:03:40 -0800

    Adding css styles to base cedar components bundle so it'll be extracted into another file

--- src/bundles/baseComponents/_index.js
+++ src/bundles/baseComponents/_index.js
@@ -13,6 +13,19 @@ import cdrRating from '@rei-co-op/cedar-rating';
 import cdrRow from '@rei-co-op/cedar-row';
 import cdrSelect from '@rei-co-op/cedar-select';
 
+import cdrAStyle from '@rei-co-op/cedar-a/dist/cdr-a.css';
+import cdrButtonStyle from '@rei-co-op/cedar-button/dist/cdr-button.css';
+import cdrCardStyle from '@rei-co-op/cedar-card/dist/cdr-card.css';
+import cdrCheckboxStyle from '@rei-co-op/cedar-checkbox/dist/cdr-checkbox.css';
+import cdrHeadingStyle from '@rei-co-op/cedar-heading/dist/cdr-heading.css';
+import cdrIconStyle from '@rei-co-op/cedar-icon/dist/cdr-icon.css';
+import cdrImgStyle from '@rei-co-op/cedar-image/dist/cdr-img.css';
+import cdrInputStyle from '@rei-co-op/cedar-input/dist/cdr-input.css';
+import cdrListStyle from '@rei-co-op/cedar-list/dist/cdr-list.css';
+import cdrRadioStyle from '@rei-co-op/cedar-radio/dist/cdr-radio.css';
+import cdrRatingStyle from '@rei-co-op/cedar-rating/dist/cdr-rating.css';
+import cdrSelectStyle from '@rei-co-op/cedar-select/dist/cdr-select.css';
+
 export default {
   cdrA,
   cdrButton,
@@ -29,3 +42,20 @@ export default {
   cdrRow,
   cdrSelect,
 };
+
+const styles = [
+  cdrAStyle,
+  cdrButtonStyle,
+  cdrCardStyle,
+  cdrCheckboxStyle,
+  cdrHeadingStyle,
+  cdrIconStyle,
+  cdrImgStyle,
+  cdrInputStyle,
+  cdrListStyle,
+  cdrRadioStyle,
+  cdrRatingStyle,
+  cdrSelectStyle,
+];
+
+export { styles };
