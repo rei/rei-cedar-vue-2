bba03e2198e8d447203a7d19524ca0982de85721
Allen Taylor <allen.taylor@paviasystems.com>
2017-12-13 16:46:03 -0800

    Updating examples for cedar list

---
+++ src/components/list/examples/demo/Styled.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <cdr-list modifier="disc">
+      <li>List item - disc</li>
+      <li>List item - disc</li>
+    </cdr-list>
+
+    <cdr-list modifier="disc compact">
+      <li>List item - disc compact</li>
+      <li>List item - disc compact</li>
+    </cdr-list>
+
+    <cdr-list modifier="circle">
+      <li>List item - circle</li>
+      <li>List item - circle</li>
+    </cdr-list>
+
+    <cdr-list modifier="circle compact">
+      <li>List item - circle compact</li>
+      <li>List item - circle compact</li>
+    </cdr-list>
+
+  </div>
+</template>
+
+<script>
+import Components from 'Components/_index';
+
+export default {
+  name: 'StyledList',
+  components: Components,
+};
+</script>
+
+<style>
+
+</style>
