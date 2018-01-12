0a527f0d99dcdd894a508b281f0ec4789c5104ee
Allen Taylor <allen.taylor@paviasystems.com>
2018-01-11 17:50:55 -0800

    Added the function to find *.vue files in the folder directory

--- build/vue-docs.js
+++ build/vue-docs.js
@@ -1,4 +1,12 @@
 const path = require('path')
+const fs = require('fs')
 const json2md = require('json2md')
 const vueDocgen = require('vue-docgen-api')
+const glob = require('glob')
+
+glob('src/**/**/*.vue', (err, files) => {
+  files.forEach((file) => {
+    console.log(file);
+  })
+})
 
