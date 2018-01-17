0815a2b863be29c022d6f367251c421171ca3bd2
Allen Taylor <allen.taylor@paviasystems.com>
2018-01-17 10:32:10 -0800

    Adding converter for markdown link to json2md module

--- build/vue-docs.js
+++ build/vue-docs.js
@@ -6,11 +6,20 @@ const glob = require('glob')
 
 let vueObjs = [];
 
+// Add convert for markdown anchor
+json2md.converters.anchor = (anchor, json) => {
+  if (!anchor.text)
+    return "";
+  
+  return `[${anchor.text}](${anchor.source})`;
+};
+
 glob('src/+(components|compositions|bundles)/**/*.vue', {ignore: ['**/node_modules/**', '**/examples/**']}, (err, files) => {
   if(err) console.log(err);
 
-  files.forEach((file, idx, arr) => {
+  files.forEach((file, idx) => {
     vueObjs.push(vueDocgen.parse(file));
+    // console.log(vueDocgen.parse(file));
   })
 })
 
