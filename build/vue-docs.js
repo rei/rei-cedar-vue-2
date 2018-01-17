50a7d12dc66c8f1c39a15a2ca422ab92ea41b409
Allen Taylor <allen.taylor@paviasystems.com>
2018-01-17 14:28:58 -0800

    Added function to create markdown table from the vue-docgen-api object representing vue component methods

--- build/vue-docs.js
+++ build/vue-docs.js
@@ -4,7 +4,7 @@ const json2md = require('json2md')
 const vueDocgen = require('vue-docgen-api')
 const glob = require('glob')
 
-let vueObjs = [];
+let json2mdTemplate = [];
 
 // Add convert for markdown anchor
 json2md.converters.anchor = (anchor, json) => {
@@ -14,12 +14,55 @@ json2md.converters.anchor = (anchor, json) => {
   return `[${anchor.text}](${anchor.source})`;
 };
 
+// pull in *.vue files from directory
 glob('src/+(components|compositions|bundles)/**/*.vue', {ignore: ['**/node_modules/**', '**/examples/**']}, (err, files) => {
   if(err) console.log(err);
 
+  // convert *.vue files into JSON objects then convert to *.md files
   files.forEach((file, idx) => {
-    vueObjs.push(vueDocgen.parse(file));
-    // console.log(vueDocgen.parse(file));
+    let vueObj = vueDocgen.parse(file);
+    
+    json2mdTemplate.concat([
+      {h2: `${vueObj.displayName}`},
+      {anchor: {text: `${file}`, source: ``}},
+      {p: `${vueObj.description}`},
+      {h3: "Props, Methods, Events, Slots"}
+    ])
   })
 })
 
+
+// auxilary function to create table from `props` property of json2md object
+function tableFromProps(propsArr) {
+  const headers = ["Prop Name", "Type", "Default", "Require", "Description"]
+  let rows = []
+
+  // construct rows of table from array of properties
+  for(const prop in propsArr) {
+    let cols = []
+    cols.push(`${prop}`) // property name
+    cols.push(prop["type"]["name"]); // type of the property
+    cols.push(prop["defaultValue"] ? prop["defaultValue"]["value"] : '') // property default value
+    cols.push(prop["required"] ? 'true' : 'false') // property is required
+    cols.push(prop["description"] || '') // description of the property
+
+    rows.push(cols)
+  }
+
+  return {headers, rows}
+}
+  
+// auxilary function to create table from `methods` property of json2md object
+function tableFromMethods(methodsArr) {
+
+}
+
+// auxilary function to create table from `events` property of json2md object
+function tableFromEvents(vueObj) {
+
+}
+
+// auxilary function to create table from `slots` property of json2md object
+function tableFromSlots(vueObj) {
+
+}
