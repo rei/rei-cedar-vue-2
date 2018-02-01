ce3dd9db5903491428b40bffb1522457cd08221a
Lance Harper <laharpe@rei.com>
2018-02-01 15:40:22 -0800

    should fail

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,7 @@ module.exports = {
 	rules: {
 		'body-leading-blank': [1, 'always'],
 		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 100],
-		'header-min-length': [2, 'always', 0],
+		'header-max-length': [2, 'always', 72],
 		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [
 			2,
