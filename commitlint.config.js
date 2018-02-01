a60b51a708b953684e06f2671b649408bee5264b
Lance Harper <laharpe@rei.com>
2018-02-01 15:19:25 -0800

    require header

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,8 @@ module.exports = {
 	rules: {
 		'body-leading-blank': [1, 'always'],
 		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 72],
+		'header-max-length': [2, 'always', 100],
+		'header-min-length': [2, 'always', 0],
 		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [
 			2,
