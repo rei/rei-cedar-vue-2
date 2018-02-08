70b5cfadb5da95a6e098a0ae1bb84ec1f4a547e2
Allen Taylor <allen.taylor@paviasystems.com>
2018-02-08 15:50:49 -0800

    chore(documentation): archiving api
    putting all API documentation files into a folder for that cedar 2 component

--- build/vue-docs-archive.js
+++ build/vue-docs-archive.js
@@ -1,46 +1,45 @@
 const path = require('path')
 const errCodes = require('errno')
 const semverDiff = require('semver-diff')
-const smever = require('semver')
-const fs = require('fs')
+const semever = require('semver')
+const fse = require('fs-extra')
 const glob = require('glob')
-const util = require('util')
+
+const ARCHIVE = 'api-archive'
 
 // put all markdown files for each component into an 'api-archive' folder
-glob('src/+(components|compositions)/**/*.vue', {ignore: ['**/node_modules/**', '**/examples/**', '**/demos/**', '**/api-archive/**']}, (mdErr, vueFiles) => {
+glob('src/+(components|compositions)/**/*.vue', {ignore: ['**/node_modules/**', '**/examples/**', '**/demos/**', `**/${ARCHIVE}/**`]}, (mdErr, vueFiles) => {
   if(mdErr) {
     throw new Error(`Error while trying to find markdown documentation files:\n${mdErr}`)
   }
 
+  console.log(`Archiving markdown documentation files`)
+
   // use Vue files to find associated markdown files
   vueFiles.forEach((file) => {
-
     const startFileName = file.lastIndexOf('/') + 1, endFileName = file.lastIndexOf('.')
     const vueCompName = file.slice(startFileName, endFileName)
     const vueCompDir = file.slice(0, startFileName)
-
-    let latestMdDoc = null, latestMdVer = '0.0.0'
+    const fullDirPath = `${path.dirname(__dirname)}/${vueCompDir}`
 
     // create 'api-archive' folder if it doesn't exist
-    checkDirectory(`${vueCompDir}api-archive`, (dirErr) => {
+    fse.ensureDir(`${fullDirPath + ARCHIVE}`, (dirErr) => {
       if(dirErr) {
-        throw new Error(`There was a problem creating an api-archive folder for ${vueCompName}:\n${dirErr}`)
+        throw new Error(`There was a problem creating a ${ARCHIVE} folder for ${vueCompName}:\n${dirErr}`)
       }
+
+      // move any markdown files into the api-archive folder
+      glob(`${fullDirPath + vueCompName}-*.md`, (mdFileErr, mdFiles) => {
+        mdFiles.forEach((mdFile) => {
+          console.log(`Archiving ${mdFile}`)
+          const starMdVer = mdFile.lastIndexOf(`${vueCompName}-`) + vueCompName.length + 1, endMdVer = mdFile.lastIndexOf('.')
+          const mdFileVer = mdFile.slice(starMdVer, endMdVer)
+          fse.move(mdFile, `${fullDirPath + ARCHIVE}/${vueCompName}-${mdFileVer}.md`, {overwrite: true}, (mvErr) =>{
+            if (mvErr) 
+              throw new Error(`Error while trying to move ${vueCompName}-${mdFileVer}.md into ${ARCHIVE}`)
+          })
+        })
+      })
     })
   })
 })
-
-// create directory if it doesn't already exit
-function checkDirectory(dir, callback) {
-  fs.stat(dir, (dirErr, stats) => {
-    // check to see if directory doesn't exist
-    if (dirErr && errCodes.errno[dirErr.errno].code === errCodes.code.ENOENT.code) {
-      fs.mkdir(dir, () => {
-        console.log(`There was a problem creating the director for ${dir}`)
-      })
-    } else {
-      // in case there's another error
-      callback(dirErr)
-    }
-  })
-}
