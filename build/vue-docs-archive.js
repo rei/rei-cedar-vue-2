241d976f7cd7aa678e52f60a1cb88f2519e8b4de
Allen Taylor <allen.taylor@paviasystems.com>
2018-02-22 11:01:04 -0800

    refactor(documentation): updated documentation process
    Changed the way markdown files are archived

--- build/vue-docs-archive.js
+++ build/vue-docs-archive.js
@@ -4,6 +4,7 @@ const semver = require('semver')
 const glob = require('glob')
 
 const ARCHIVE = 'api-archive'
+const VERSIONS = 'versions'
 
 // put all markdown files for each component into an 'api-archive' folder
 glob('src/+(components|compositions)/**/*.vue', {ignore: ['**/node_modules/**', '**/examples/**', '**/demos/**', `**/${ARCHIVE}/**`]}, (mdErr, vueFiles) => {
@@ -21,9 +22,9 @@ glob('src/+(components|compositions)/**/*.vue', {ignore: ['**/node_modules/**', 
     const fullDirPath = `${path.dirname(__dirname)}/${vueCompDir}`
 
     // create 'api-archive' folder if it doesn't exist
-    fs.ensureDir(`${fullDirPath + ARCHIVE}`, (dirErr) => {
+    fs.ensureDir(`${fullDirPath + ARCHIVE}/${VERSIONS}`, (dirErr) => {
       if(dirErr) {
-        throw new Error(`There was a problem creating a ${ARCHIVE} folder for ${vueCompName}:\n${dirErr}`)
+        throw new Error(`There was a problem creating a ${ARCHIVE}/${VERSIONS} folder for ${vueCompName}:\n${dirErr}`)
       }
 
       // move any markdown files into the api-archive folder
@@ -37,11 +38,21 @@ glob('src/+(components|compositions)/**/*.vue', {ignore: ['**/node_modules/**', 
           const starMdVer = mdFile.lastIndexOf(`${vueCompName}-`) + vueCompName.length + 1, endMdVer = mdFile.lastIndexOf('.')
           const mdFileVer = mdFile.slice(starMdVer, endMdVer)
           
-          console.log(`Moving ${fullDirPath + ARCHIVE}/${vueCompName}${(semver.valid(mdFileVer)) ? `-${mdFileVer}` : ``}.md\n`)
-          fs.move(mdFile, `${fullDirPath + ARCHIVE}/${vueCompName}${(semver.valid(mdFileVer)) ? `-${mdFileVer}` : ``}.md`, {overwrite: true}, (mvErr) => {
-            if (mvErr) 
-              throw new Error(`Error while trying to move ${vueCompName}-${mdFileVer}.md into ${ARCHIVE}`)
-          })
+          // put versioned markdown documentation files in their own 'versions' folder seperate from the most current version
+          if(semver.valid(mdFileVer)) {
+            fs.move(mdFile, `${fullDirPath + ARCHIVE}/${VERSIONS}/${vueCompName}-${mdFileVer}.md`, {overwrite: true}, (mvErr) => {
+              if (mvErr) 
+                throw new Error(`Error while trying to move ${vueCompName}-${mdFileVer}.md into ${ARCHIVE}/${VERSIONS}`)
+            })
+            console.log(`Moving ${fullDirPath + ARCHIVE}/${VERSIONS}/${vueCompName}-${mdFileVer}.md\n`)
+          }
+          else {
+            fs.move(mdFile, `${fullDirPath + ARCHIVE}/${vueCompName}.md`, {overwrite: true}, (mvErr) => {
+              if (mvErr) 
+                throw new Error(`Error while trying to move ${vueCompName}.md into ${ARCHIVE}`)
+            })
+            console.log(`Moving ${fullDirPath + ARCHIVE}/${vueCompName}.md\n`)
+          }
         })
       })
     })
