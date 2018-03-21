5b8ac0f6c09bdc93decd259c54b67e3b5b3754ef
Lance Harper <laharpe@rei.com>
2018-03-21 13:44:18 -0700

    chore(assets): add script for creating icon object in cdr-assets
    affects: @rei/cdr-assets

--- src/cdr-assets/build.js
+++ src/cdr-assets/build.js
@@ -2,15 +2,12 @@
 
 const chalk = require('chalk');
 const fs = require('fs-extra');
+const klaw = require('klaw');
+const through2 = require('through2');
 const path = require('path');
 const { exec } = require('child_process');
 
-const icons = require('require-all')({
-  dirname: path.resolve(__dirname, '..', 'assets/icons/rei'),
-});
-
-console.log(icons);
-
+// core-css variables
 const distDir = path.resolve(__dirname, 'dist');
 const cssDir = path.resolve(__dirname, '..', 'css');
 const staticDir = path.resolve(__dirname, '../..', 'static');
@@ -19,7 +16,8 @@ const staticDir = path.resolve(__dirname, '../..', 'static');
 fs.emptyDirSync(distDir);
 
 // Compile css
-exec(`postcss ${cssDir}/main.postcss -o ${distDir}/cdr-core.css`, () => {
+exec(`postcss ${cssDir}/main.postcss -o ${distDir}/cdr-core.css`, (err) => {
+  if (err) throw err;
   console.log(chalk.green('Compiled cdr-core.css')); // eslint-disable-line no-console
 });
 
@@ -38,4 +36,27 @@ fs.copySync(staticDir, distDir, {
   },
 });
 
-// TODO: copy variable files
+// make icon index
+const icons = {};
+
+const onlySVG = through2.obj(function filterSVG(item, enc, next) {
+  if (!item.stats.isDirectory() && path.extname(item.path) === '.svg') this.push(item);
+  next();
+});
+
+klaw(path.resolve(__dirname, '..', 'assets/icons/rei'))
+  .pipe(onlySVG)
+  .on('data', (item) => {
+    const name = path.basename(item.path).split('.')[0];
+    fs.readFile(item.path, 'utf8', (err, data) => {
+      if (err) throw err;
+
+      Object.assign(icons, { [`${name}`]: data });
+    });
+  })
+  .on('end', () => {
+    fs.writeFile('./dist/icons.json', JSON.stringify(icons, null, '\t'), 'utf8', (err) => {
+      if (err) throw err;
+      console.log(chalk.green('Compiled icons.json'));// eslint-disable-line no-console
+    });
+  });
