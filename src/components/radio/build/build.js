8669917d8af6ca90adc5f05fa781b25c076a368d
Lance Harper <laharpe@rei.com>
2018-03-30 10:21:05 -0700

    chore(build): externalize lodash
    affects: @rei/cdr-radio, @rei/cdr-select

--- src/components/radio/build/build.js
+++ src/components/radio/build/build.js
@@ -7,8 +7,10 @@ Externalize other cdr-* packages
 This assumes packages are listed as peerDependencies so they don't need to be bundled in
 */
 const compWebpack = {
-  // TODO: lodash
-  externals: /^@rei\/cdr-/,
+  externals: [
+    /^@rei\/cdr-/,
+    /^lodash/,
+  ],
 };
 
 build(info, {}, compWebpack);
