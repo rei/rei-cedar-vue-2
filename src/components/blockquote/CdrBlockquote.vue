4155192a8b6fb7d4bfe906d0c911839d7889cbd8
mhewson <mhewson@gmail.com>
2018-03-30 22:38:41 -0700

    feat(component): adding an additional slot to the quote elemement as it is valid markup to nest bloc
    affects: @rei/cdr-quote

---
+++ src/components/blockquote/CdrBlockquote.vue
@@ -0,0 +1,31 @@
+<template>
+  <blockquote class="cdr-blockquote cdr-inset--squish">
+    <p
+      v-if="summary"
+      class="cdr-blockquote__summary">
+      {{ summary }}
+    </p>
+    <cite
+      v-if="attribution"
+      class="cdr-text--attribution">
+      {{ attribution }}
+    </cite>
+  </blockquote>
+</template>
+
+<script>
+export default {
+  name: 'CdrBlockquote',
+  props: {
+    /** Blockquote summary text */
+    summary: String,
+    /** Blockquote credit text */
+    attribution: String,
+  },
+};
+</script>
+
+<style>
+  @import '../../css/settings/_index.pcss';
+  @import './styles/CdrBlockquote.pcss';
+</style>
