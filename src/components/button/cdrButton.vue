8e3652c7e87432c24c2d46501a1439d7701a0daf
Darin Trimillos <darin@trimillos.com>
2018-04-10 16:35:39 -0700

    WIP: updated examples and styling to better match spec
    affects: @rei/cdr-button
    
    Medium-sized button is now the default. Use 'lg' & 'sm' to change size. Button padding is still
    incorrect. Icons in icon buttons wrong size.

--- src/components/button/cdrButton.vue
+++ src/components/button/cdrButton.vue
@@ -18,7 +18,7 @@ import modifier from 'mixinsdir/modifier';
  *
  * <span class="modifiers">Modifiers</span>
  * Modifiers can be combined 1 from each grouping.
- * {secondary} | {sm,xs} | {block, fixed, responsive} **OR** link
+ * {secondary} | {sm,lg} | {block, fixed, responsive} **OR** link
  * Use of the 'link' modifier depends on including the css for cdrA.
  * A modifier list that contains 'link' exposes the same modifiers as cdrA and the other modifiers shouldn't be combined with it.
  * Responsive makes the button full width and block @ sm breakpoint.
--- src/components/button/examples/demo/Default.vue
+++ src/components/button/examples/demo/Default.vue
@@ -1,34 +1,105 @@
 <template>
   <div>
     <div class="button-example">
+      <h2>Default buttons with size modifiers</h2>
+      <cdr-button
+        type="submit"
+        modifier="lg"
+      >Large {lg}
+      </cdr-button>
       <cdr-button
         data-backstop="cdr-button"
         type="submit"
         :on-click="log"
-      >
-        Default with click
+      >Medium
       </cdr-button>
-      <cdr-button>
+      <cdr-button
+        type="submit"
+        modifier="sm"
+      >Small {sm}
+      </cdr-button>
+      <cdr-button
+        type="submit"
+        modifier="lg"
+        disabled
+      >Large
+      </cdr-button>
+      <cdr-button
+        type="submit"
+        disabled
+      >Medium
+      </cdr-button>
+      <cdr-button
+        type="submit"
+        modifier="sm"
+        disabled
+      >Small
+      </cdr-button>
+    </div>
+    <div class="button-example">
+      <h2>Icon buttons with size modifiers</h2>
+      <cdr-button
+        type="submit"
+        modifier="lg">
         <cdr-icon
           url="/static/rei-icons.svg#cdr-check-lg"
           modifier="sm"
         />
-        Button with icon
+        Large {lg}
       </cdr-button>
-      <cdr-button disabled>Default Disabled</cdr-button>
-    </div>
-    <div class="button-example">
+      <cdr-button
+        data-backstop="cdr-button"
+        type="submit"
+        :on-click="log">
+        <cdr-icon
+          url="/static/rei-icons.svg#cdr-check-lg"
+          modifier="sm"
+        />
+        Medium
+      </cdr-button>
+      <cdr-button
+        type="submit"
+        modifier="sm">
+        <cdr-icon
+          url="/static/rei-icons.svg#cdr-check-lg"
+          modifier="sm"
+        />
+        Small {sm}
+      </cdr-button>
+      <cdr-button
+        type="submit"
+        modifier="lg"
+        disabled>
+        <cdr-icon
+          url="/static/rei-icons.svg#cdr-check-lg"
+          modifier="sm"
+        />
+        Large {lg}
+      </cdr-button>
+      <cdr-button
+        data-backstop="cdr-button"
+        type="submit"
+        :on-click="log"
+        disabled>
+        <cdr-icon
+          url="/static/rei-icons.svg#cdr-check-lg"
+          modifier="sm"
+        />
+        Medium
+      </cdr-button>
       <cdr-button
         type="submit"
         modifier="sm"
-      >Small button</cdr-button>
-      <cdr-button
-        type="submit"
-        modifier="xs"
-      >Extra Small button</cdr-button>
+        disabled>
+        <cdr-icon
+          url="/static/rei-icons.svg#cdr-check-lg"
+          modifier="sm"
+        />
+        Small {sm}
+      </cdr-button>
     </div>
-
     <div class="button-example">
+      <h2>Color themes</h2>
       <cdr-button theme="red">red</cdr-button>
       <cdr-button
         theme="red"
