dcefd4ec3cc8b06d6d77d89d722782a6f6e25d16
Lance Harper <laharpe@rei.com>
2018-04-18 14:58:20 -0700

    feat(icons): gernerate icon components
    affects: @rei/cdr-icon
    
    each svg icon is created as it's own vue component. SVG sprite creation is handled by icon and a new
    sprite component has been added.
    
    BREAKING CHANGE:
    cdr-icon is now used as a lower level component for the icon components. A complete rewrite with no
    backwards compatibility.

---
+++ src/components/icon/comps/bookmark-stroke.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- File generated during build -->
+  <!-- eslint-disable -->
+  <cdr-icon v-bind="$props">
+    <path d="M5 2h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1.6.8L12.002 17 5.6 21.8A1 1 0 0 1 4 21V3a1 1 0 0 1 1-1zm1 2v15l5.402-4.05a1 1 0 0 1 1.2 0L18 19V4H6z"></path>
+  </cdr-icon>
+</template>
+
+<script>
+import CdrIcon from 'componentsdir/icon/CdrIcon';
+
+export default {
+  name: 'IconBookmarkStroke',
+  components: {
+    CdrIcon,
+  },
+  extends: CdrIcon,
+};
+</script>
