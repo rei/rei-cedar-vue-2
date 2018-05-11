36810ef7ca48cde53cbb4a756be9965f0d3981a1
Darin Trimillos <darin@trimillos.com>
2018-05-11 11:42:09 -0700

    WIP: added button with icon comps to proving grounds
    affects: @rei/cdr-button, @rei/cdr-cta

--- src/components/button/examples/Buttons.vue
+++ src/components/button/examples/Buttons.vue
@@ -3,21 +3,21 @@
     <h2>Buttons</h2>
     <default-buttons/>
     <secondary-buttons/>
-    <!--<icon-buttons />-->
+    <icon-buttons />
   </div>
 </template>
 
 <script>
 import defaultButtons from './demo/Default';
 import secondaryButtons from './demo/Secondary';
-// import iconButtons from './deom/Icons';
+import iconButtons from './demo/Icons';
 
 export default {
   name: 'Buttons',
   components: {
     defaultButtons,
     secondaryButtons,
-    // iconButtons,
+    iconButtons,
   },
 };
 </script>
---
+++ src/components/button/examples/demo/Icons.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="button-example">
+    <h2>CdrButton + CdrIcon Comps</h2>
+    <cdr-button
+      size="large"
+    >
+      <icon-check-lg />
+      Button and Icon
+    </cdr-button>
+    <cdr-button
+      size="medium"
+      style-modifiers="secondary"
+    >
+      <icon-check-lg />
+      Button and Icon
+    </cdr-button>
+    <cdr-button
+      size="small"
+    >
+      <icon-check-sm />
+      Button and Icon
+    </cdr-button>
+  </div>
+</template>
+
+<script>
+import { CdrButton } from 'componentsdir/button/dist/cdr-button';
+import { IconCheckLg, IconCheckSm } from 'componentsdir/icon/dist/cdr-icon';
+
+export default {
+  name: 'ButtonIconComps',
+  components: {
+    CdrButton,
+    IconCheckLg,
+    IconCheckSm,
+  },
+};
+</script>
+
+<style>
+  .button-example {
+    padding: 10px;
+  }
+</style>
