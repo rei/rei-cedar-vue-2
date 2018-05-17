02b9697f791b9c86eca744bf946597e242fcc106
Darin Trimillos <darin@trimillos.com>
2018-05-17 14:51:59 -0700

    WIP: tied button icon styles to a new class because css modules are coming
    affects: @rei/cdr-button

--- src/components/button/examples/demo/Icons.vue
+++ src/components/button/examples/demo/Icons.vue
@@ -7,40 +7,55 @@
         href="https://rei.com"
         size="large"
       ><icon-check-lg
+        class="cdr-button__icon"
       />Anchor and Icon</cdr-button
       >
       <cdr-button
         size="medium"
         style-modifiers="secondary"
       ><icon-check-lg
+        class="cdr-button__icon"
       />Medium and Icon</cdr-button
       >
       <cdr-button
         size="small"
       ><icon-check-lg
         modifier="sm"
+        class="cdr-button__icon"
       />Icon CSS small</cdr-button
       >
       <cdr-button
         size="small"
       ><icon-check-sm
+        class="cdr-button__icon"
       />Small (different) icon</cdr-button
       >
     </div>
     <div class="button-example">
+      <h2>Using a sprite</h2>
       <cdr-button
         size="large"
-      ><icon-clock
+      ><cdr-icon
+        class="cdr-button__icon"
+        use="#download"
+        modifier="inherit-color"
       />Button and Icon</cdr-button
       >
       <cdr-button
         size="medium"
-      ><icon-clock
+        style-modifiers="secondary"
+      ><cdr-icon
+        class="cdr-button__icon"
+        use="#twitter"
+        modifier="inherit-color"
       />Button and Icon</cdr-button
       >
       <cdr-button
         size="small"
-      ><icon-clock
+      ><cdr-icon
+        class="cdr-button__icon"
+        use="#instagram"
+        modifier="inherit-color"
       />Button and Icon</cdr-button
       >
     </div>
@@ -49,6 +64,7 @@
         size="small"
         :responsive-size="['large@extra-small', 'medium@small', 'small@large']"
       >Responsive<icon-clock
+        class="cdr-button__icon"
       />
       </cdr-button>
     </div>
@@ -57,8 +73,9 @@
 
 <script>
 import Components from 'componentsdir/_index';
-import { CdrButton } from 'componentsdir/button/dist/cdr-button';
-import { IconCheckLg, IconCheckSm, IconClock, CdrIcon } from 'componentsdir/icon/dist/cdr-icon';
+import CdrButton from 'componentsdir/button/CdrButton';
+import { IconCheckLg, IconCheckSm, IconClock } from 'componentsdir/icon/dist/cdr-icon';
+import CdrIcon from 'componentsdir/icon/CdrIcon';
 
 export default {
   name: 'ButtonIconComps',
