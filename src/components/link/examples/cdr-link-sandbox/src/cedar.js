ace28a3ab096fb0c1a69ed7ba0ae31e1390b642f
mhewson <mhewson@gmail.com>
2018-05-25 09:15:53 -0700

    docs(sandbox): adding a codesandbox instance to the examples for link
    affects: cdr-link, @rei/cdr-link

---
+++ src/components/link/examples/cdr-link-sandbox/src/cedar.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/first */
+
+export { CdrCol } from "@rei/cdr-col";
+
+export { CdrRow } from "@rei/cdr-row";
+
+export { CdrText } from "@rei/cdr-text";
+
+export { CdrLink } from "@rei/cdr-link";
+
+import "@rei/cdr-link/dist/cdr-link.css";
---
+++ src/components/link/examples/cdr-link-sandbox/src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+
+import "@rei/cdr-assets/dist/cdr-core.css";
+import "@rei/cdr-assets/dist/cdr-fonts.css";
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});
