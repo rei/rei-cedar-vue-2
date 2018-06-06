d3ef974a6ccf29b1b3ade099c1b944f2010afa6f
Darin Trimillos <darin@trimillos.com>
2018-06-06 10:31:14 -0700

    WIP(button): removed icon-only-button as a standalone component
    icon-only-button functionality rolled back into cdr-button

--- src/components/iconOnlyButton/build/main.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-export { default as CdrIconOnlyButton } from 'componentsdir/iconOnlyButton/CdrIconOnlyButton';
-export { default as CdrCloseButton } from 'componentsdir/iconOnlyButton/comps/CdrCloseButton';
-export { default as CdrPlayButton } from 'componentsdir/iconOnlyButton/comps/CdrPlayButton';
