6e3fe4d3d924a868673e2f5c033ecc00da98c73c
Allen Taylor <allen.taylor@paviasystems.com>
2018-06-21 10:11:22 -0700

    WIP(work in progress): create skeleton table with styling
    affects: @rei/cdr-table

--- src/components/table/CdrTable.vue
+++ src/components/table/CdrTable.vue
@@ -1,11 +1,27 @@
 <template>
   <!-- disable lint errors on line length in template -->
   <!-- eslint-disable max-len -->
+  <table class="cdr-table">
+    <!-- @slot table placed into data table component -->
+    <slot />
+  </table>
 </template>
 
 <script>
+import modifier from 'mixinsdir/modifier';
+
+/**
+ * Cedar 2 compfor for data table
+ * @author [REI Software Engineering](https://rei.github.io/rei-cedar/)
+ */
 export default {
   name: 'CdrTable',
+  mixins: [modifier],
+  computed: {
+    baseClass() {
+      return 'cdr-table';
+    },
+  },
 };
 </script>
 
--- src/components/table/examples/Table.vue
+++ src/components/table/examples/Table.vue
@@ -1,12 +1,63 @@
 <template>
   <div>
     <h2>table</h2>
+    <cdr-table>
+      <thead>
+        <tr>
+          <th/>
+          <th>one</th>
+          <th>two</th>
+          <th>three</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+      </tbody>
+    </cdr-table>
   </div>
 </template>
 
 <script>
+import Components from 'componentsdir/_index';
+
 export default {
   name: 'Table',
+  components: Components,
 };
 </script>
 
