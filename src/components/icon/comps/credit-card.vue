5bd063f55fe55688460f9feecb6c755a35fbfac7
Lance Harper <laharpe@rei.com>
2018-06-28 15:48:14 -0700

    feat(icon): add a slot to all components
    affects: @rei/cdr-icon

--- src/components/icon/comps/credit-card.vue
+++ src/components/icon/comps/credit-card.vue
@@ -2,6 +2,7 @@
   <!-- File generated during build -->
   <!-- eslint-disable -->
   <cdr-icon v-bind="$props">
+    <slot />
     <path d="M3 5h18a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1zm17 4V7H4v2h16zm0 3H4v5h16v-5zm-4 2h3v2h-3v-2zm-3 0h2v2h-2v-2z"></path>
   </cdr-icon>
 </template>
