51d4611e7f0a34e10e74598a1ea64047522e2872
Allen Taylor <allen.taylor@paviasystems.com>
2018-07-03 11:51:20 -0700

    WIP(data table): make tables compact in padding with modifier
    affects: @rei/cdr-table

--- src/components/table/examples/Table.vue
+++ src/components/table/examples/Table.vue
@@ -464,6 +464,182 @@
         </tr>
       </tbody>
     </cdr-table>
+
+    <h2>compact table</h2>
+    <cdr-table modifier="compact">
+      <thead>
+        <tr>
+          <th/>
+          <th>one</th>
+          <th>two</th>
+          <th>three</th>
+          <th>four</th>
+          <th>five</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+      </tbody>
+    </cdr-table>
+
+    <h2>borderless compact table</h2>
+    <cdr-table modifier="compact borderless">
+      <thead>
+        <tr>
+          <th/>
+          <th>one</th>
+          <th>two</th>
+          <th>three</th>
+          <th>four</th>
+          <th>five</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">row header</th>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+          <td>
+            test
+          </td>
+        </tr>
+      </tbody>
+    </cdr-table>
   </div>
 </template>
 
