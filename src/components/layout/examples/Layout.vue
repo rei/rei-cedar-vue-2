fe203cc9b428508ae5c4dda1c809c086f04393b5
Lance Harper <laharpe@rei.com>
2018-09-19 12:27:42 -0700

    WIP(layout): more space examples
    affects: @rei/cdr-layout

--- src/components/layout/examples/Layout.vue
+++ src/components/layout/examples/Layout.vue
@@ -3,6 +3,7 @@
     <h2>layout</h2>
 
     <h3>inline</h3>
+    <p>1-x</p>
     <cdr-layout
       spacing="1-x"
     >
@@ -11,10 +12,30 @@
       <cdr-button>button3</cdr-button>
     </cdr-layout>
 
+    <p>half-x</p>
+    <cdr-layout
+      spacing="half-x"
+    >
+      <cdr-button>button1</cdr-button>
+      <cdr-button>button2</cdr-button>
+      <cdr-button>button3</cdr-button>
+    </cdr-layout>
+
     <h3>stack</h3>
+    <p>1-x</p>
     <cdr-layout
       direction="stack"
       spacing="1-x"
+    >
+      <cdr-button>button1</cdr-button>
+      <cdr-button>button2</cdr-button>
+      <cdr-button>button3</cdr-button>
+    </cdr-layout>
+
+    <p>half-x</p>
+    <cdr-layout
+      direction="stack"
+      spacing="half-x"
     >
       <cdr-button>button1</cdr-button>
       <cdr-button>button2</cdr-button>
