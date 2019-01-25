437f70131d0e234d4cf146a14d90dae6e30a1a4f
Lance Harper <laharpe@rei.com>
2019-01-25 10:36:58 -0800

    chore(dependencies): update postcss dependencies, fix unit testing errors, add dark theme backstop t

--- src/components/themer/CdrThemer.backstop.js
+++ src/components/themer/CdrThemer.backstop.js
@@ -1,4 +1,11 @@
-module.exports = [{
-  label: 'Themer',
-  selectors: [],
-}];
+module.exports = [
+  {
+    label: 'Themer',
+    selectors: [],
+  },
+  {
+    label: 'Themer--dark',
+    url: 'http://localhost:3000/#/?global-theme=dark',
+    selectors: [],
+  },
+];
