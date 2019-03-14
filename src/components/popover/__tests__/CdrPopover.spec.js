02dce4492331be506511f009f1db18d2298c270d
Darin Trimillos <darin@trimillos.com>
2019-03-14 13:13:18 -0700

    test(popover): add unit tests
    affects: @rei/cdr-popover

--- src/components/popover/__tests__/CdrPopover.spec.js
+++ src/components/popover/__tests__/CdrPopover.spec.js
@@ -1,8 +1,54 @@
-import { shallow } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import CdrPopover from 'componentsdir/popover/CdrPopover';
+import CdrButton from 'componentsdir/button/CdrButton';
 
 describe('CdrPopover.vue', () => {
-  it('has a failing test by default so you remember to do them', () => {
-    expect(false).toBe(true);
+  describe('computed props', () => {
+    it('defaultStylesClass', () => {
+      const wrapper = shallowMount(CdrPopover, {
+        propsData: {
+          hasDefaultStyling: false
+        }
+      });
+      expect(wrapper.vm.defaultStylesClass).toBe('');
+    });
+  });
+
+  describe('methods', () => {
+    it('open', () => {
+      const wrapper = shallowMount(CdrPopover);
+      wrapper.vm.open();
+      expect(wrapper.vm.isOpen).toBe(true);
+    });
+
+    it('close', () => {
+      const wrapper = shallowMount(CdrPopover);
+      wrapper.vm.close();
+      expect(wrapper.vm.isOpen).toBe(false);
+    });
+
+    it('delegateClickMethod', () => {
+      const buttonWrapper = shallowMount(CdrButton, {
+        attrs: {
+          'js-data': 'popover-toggle-button'
+        },
+        attachToDocument: true
+      });      
+
+      const wrapper = shallowMount(CdrPopover, {
+        propsData: {
+          toggleButtonSelector: "[js-data='popover-toggle-button']"
+        },
+        attachToDocument: true
+      });
+
+      buttonWrapper.trigger('click');
+      expect(wrapper.vm.isOpen).toBe(true);
+
+      buttonWrapper.trigger('click');
+      expect(wrapper.vm.isOpen).toBe(false);
+
+      wrapper.destroy();
+    });
   });
 });
