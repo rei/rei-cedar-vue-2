9a42abc0c26a2724ad82b34357e512375fff384b
mhewson <mhewson@gmail.com>
2019-04-02 13:54:39 -0700

    feat(utilities): space utilities and examples updated to use token naming

--- src/components/Utilities/demos/inset.vue
+++ src/components/Utilities/demos/inset.vue
@@ -1,84 +1,25 @@
 <template>
-  <div data-backstop="inset-space-utilities">
+  <div data-backstop="inline-space-utilities">
     <cdr-text
       tag="h2"
       modifier="heading-medium"
     >
-      Inset classes
+      Inset space classes for all around padding
     </cdr-text>
-
-    <cdr-card class="example">
-      <cdr-text class="cdr-space-inset-eighth-x">
-        cdr-space-inset-eighth-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-quarter-x">
-        cdr-space-inset-quarter-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-half-x">
-        cdr-space-inset-half-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-three-quarter-x">
-        cdr-space-inset-three-quarter-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-one-x">
-        cdr-space-inset-one-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-one-and-a-half-x">
-        cdr-space-inset-one-and-a-half-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-two-x">
-        cdr-space-inset-two-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-four-x">
-        cdr-space-inset-four-x
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-eighth-x-squish">
-        cdr-space-inset-eighth-x-squish
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-quarter-x-squish">
-        cdr-space-inset-quarter-x-squish
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-half-x-squish">
-        cdr-space-inset-half-x-squish
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-three-quarter-x-squish">
-        cdr-space-inset-three-quarter-x-squish
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-one-x-squish">
-        cdr-space-inset-one-x-squish
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-two-x-squish">
-        cdr-space-inset-two-x-squish
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-four-x-squish">
-        cdr-space-inset-four-x-squish
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-eighth-x-stretch">
-        cdr-space-inset-eighth-x-stretch
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-quarter-x-stretch">
-        cdr-space-inset-quarter-x-stretch
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-half-x-stretch">
-        cdr-space-inset-half-x-stretch
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-three-quarter-x-stretch">
-        cdr-space-inset-three-quarter-x-stretch
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-one-x-stretch">
-        cdr-space-inset-one-x-stretch
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-one-and-a-half-x-stretch">
-        cdr-space-inset-one-and-a-half-x-stretch
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-two-x-stretch">
-        cdr-space-inset-two-x-stretch
-      </cdr-text>
-      <cdr-text class="cdr-space-inset-four-x-stretch">
-        cdr-space-inset-four-x-stretch
-      </cdr-text>
-
-    </cdr-card>
+    <ul>
+      <li
+        v-for="item in space"
+        :key="item.id"
+        class="example cdr-space-mb-one-x"
+      >
+        <div
+          class="space-example"
+          :class="[item]"
+        >
+          <span>{{ item }}</span>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -87,17 +28,50 @@
 import Components from 'componentsdir/_index';
 
 export default {
-  name: 'UtilitiesSpaceInset',
+  name: 'UtilitiesSpaceInline',
   components: Components,
+  data() {
+    return {
+      space: [
+        'cdr-space-inset-eighth-x',
+        'cdr-space-inset-eighth-x-squish',
+        'cdr-space-inset-eighth-x-stretch',
+        'cdr-space-inset-quarter-x',
+        'cdr-space-inset-quarter-x-squish',
+        'cdr-space-inset-quarter-x-stretch',
+        'cdr-space-inset-half-x',
+        'cdr-space-inset-half-x-squish',
+        'cdr-space-inset-half-x-stretch',
+        'cdr-space-inset-three-quarter-x',
+        'cdr-space-inset-three-quarter-x-squish',
+        'cdr-space-inset-three-quarter-x-stretch',
+        'cdr-space-inset-one-x',
+        'cdr-space-inset-one-x-squish',
+        'cdr-space-inset-one-x-stretch',
+        'cdr-space-inset-one-and-a-half-x',
+        'cdr-space-inset-one-and-a-half-x-squish',
+        'cdr-space-inset-one-and-a-half-x-stretch',
+        'cdr-space-inset-two-x',
+        'cdr-space-inset-two-x-squish',
+        'cdr-space-inset-two-x-stretch',
+        'cdr-space-inset-four-x',
+        'cdr-space-inset-four-x-squish',
+        'cdr-space-inset-four-x-stretch',
+      ],
+    };
+  },
 };
+
 </script>
 
-<style>
-  .example {
-    background-color: rgba(27, 28, 28, 0.6);
-  }
+<style >
+.example {
+  border: 1px solid;
+}
 
-  .example > * {
-    background-color: rgba(249, 229, 229, 0.8);
-  }
+.space-example::before {
+  background-color: black;
+  display: block;
+  height: auto;
+}
 </style>
