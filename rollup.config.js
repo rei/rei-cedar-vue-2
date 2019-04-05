b3cd039f53c184c91337d7f692238d87a0265a09
Darin Trimillos <darin@trimillos.com>
2019-04-05 09:00:50 -0700

    WIP(build): add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+/*
+rollup bundles for cjs, esm
+*/
+import path from 'path';
+import vue from 'rollup-plugin-vue';
+import commonjs from 'rollup-plugin-commonjs';
+import multiEntry from 'rollup-plugin-multi-entry';
+import alias from 'rollup-plugin-alias';
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+export default [
+  {
+    input: {
+      include: ['src/components/_index.js'],
+      exclude: [
+        'src/components/**/examples/*',
+        'src/components/**/examples/demo/*',
+        'src/components/**/comps/*',
+        'src/components/Utilities/*',
+        // 'src/components/_index.js',
+        'src/components/examples.js',
+      ],
+    },
+    output: [
+      {
+        file: 'dist/cedar.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/cedar.esm.js',
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      multiEntry(),
+      alias({
+        resolve: ['.vue', '.json', '.js'],
+        srcdir: resolve('src'),
+        cssdir: resolve('src/css'),
+        assetsdir: resolve('src/assets'),
+        componentsdir: resolve('src/components'),
+        compositionsdir: resolve('src/compositions'),
+        directivesdir: resolve('src/directives'),
+        mixinsdir: resolve('src/mixins'),
+      }),
+      commonjs(),
+      vue(),
+    ],
+  },
+];
--- src/components/_index.js
+++ src/components/_index.js
@@ -1,51 +1,55 @@
-import CdrLink from 'componentsdir/link/CdrLink';
-import CdrBreadcrumb from 'componentsdir/breadcrumb/CdrBreadcrumb';
-import CdrButton from 'componentsdir/button/CdrButton';
-import CdrCard from 'componentsdir/card/CdrCard';
-import CdrCheckbox from 'componentsdir/checkbox/CdrCheckbox';
-import CdrCol from 'componentsdir/grid/CdrCol';
-import CdrCta from 'componentsdir/cta/CdrCta';
-import CdrIcon from 'componentsdir/icon/CdrIcon';
-import CdrImg from 'componentsdir/image/CdrImg';
-import CdrInput from 'componentsdir/input/CdrInput';
-import CdrList from 'componentsdir/list/CdrList';
-import CdrPagination from 'componentsdir/pagination/CdrPagination';
-import CdrQuote from 'componentsdir/quote/CdrQuote';
-import CdrRadio from 'componentsdir/radio/CdrRadio';
-import CdrRating from 'componentsdir/rating/CdrRating';
-import CdrRow from 'componentsdir/grid/CdrRow';
-import CdrSelect from 'componentsdir/select/CdrSelect';
-import CdrTabPanel from 'componentsdir/tabs/CdrTabPanel';
-import CdrTabs from 'componentsdir/tabs/CdrTabs';
-import CdrText from 'componentsdir/text/CdrText';
-import CdrThemer from 'componentsdir/themer/CdrThemer';
-import CdrDataTable from 'componentsdir/dataTable/CdrDataTable';
-import CdrAccordion from 'componentsdir/accordion/CdrAccordion';
-import CdrAccordionItem from 'componentsdir/accordion/CdrAccordionItem';
+// import CdrLink from 'componentsdir/link/CdrLink';
+// import CdrBreadcrumb from 'componentsdir/breadcrumb/CdrBreadcrumb';
+// import CdrButton from 'componentsdir/button/CdrButton';
+// import CdrCard from 'componentsdir/card/CdrCard';
+// import CdrCheckbox from 'componentsdir/checkbox/CdrCheckbox';
+// import CdrCol from 'componentsdir/grid/CdrCol';
+// import CdrCta from 'componentsdir/cta/CdrCta';
+// import CdrIcon from 'componentsdir/icon/CdrIcon';
+// import CdrImg from 'componentsdir/image/CdrImg';
+// import CdrInput from 'componentsdir/input/CdrInput';
+// import CdrList from 'componentsdir/list/CdrList';
+// import CdrPagination from 'componentsdir/pagination/CdrPagination';
+// import CdrQuote from 'componentsdir/quote/CdrQuote';
+// import CdrRadio from 'componentsdir/radio/CdrRadio';
+// import CdrRating from 'componentsdir/rating/CdrRating';
+// import CdrRow from 'componentsdir/grid/CdrRow';
+// import CdrSelect from 'componentsdir/select/CdrSelect';
+// import CdrTabPanel from 'componentsdir/tabs/CdrTabPanel';
+// import CdrTabs from 'componentsdir/tabs/CdrTabs';
+// import CdrText from 'componentsdir/text/CdrText';
+// import CdrThemer from 'componentsdir/themer/CdrThemer';
+// import CdrDataTable from 'componentsdir/dataTable/CdrDataTable';
+// import CdrAccordion from 'componentsdir/accordion/CdrAccordion';
+// import CdrAccordionItem from 'componentsdir/accordion/CdrAccordionItem';
 
-export default {
-  CdrAccordion,
-  CdrAccordionItem,
-  CdrLink,
-  CdrBreadcrumb,
-  CdrButton,
-  CdrCard,
-  CdrCheckbox,
-  CdrCol,
-  CdrCta,
-  CdrIcon,
-  CdrImg,
-  CdrInput,
-  CdrList,
-  CdrPagination,
-  CdrQuote,
-  CdrRadio,
-  CdrRating,
-  CdrRow,
-  CdrSelect,
-  CdrTabPanel,
-  CdrTabs,
-  CdrText,
-  CdrThemer,
-  CdrDataTable,
-};
+export { default as CdrLink } from 'componentsdir/link/CdrLink';
+export { default as CdrButton } from 'componentsdir/button/CdrButton';
+export { default as CdrCta } from 'componentsdir/cta/CdrCta';
+
+// export default {
+//   CdrAccordion,
+//   CdrAccordionItem,
+//   CdrLink,
+//   CdrBreadcrumb,
+//   CdrButton,
+//   CdrCard,
+//   CdrCheckbox,
+//   CdrCol,
+//   CdrCta,
+//   CdrIcon,
+//   CdrImg,
+//   CdrInput,
+//   CdrList,
+//   CdrPagination,
+//   CdrQuote,
+//   CdrRadio,
+//   CdrRating,
+//   CdrRow,
+//   CdrSelect,
+//   CdrTabPanel,
+//   CdrTabs,
+//   CdrText,
+//   CdrThemer,
+//   CdrDataTable,
+// };
