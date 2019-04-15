87558e9d31db6306e0a4d6aed42620732c51beb0
Darin Trimillos <darin@trimillos.com>
2019-04-15 10:26:26 -0700

    refactor(rollup.config.js): one rollup config to rule them all

--- rollup.config.js
+++ rollup.config.js
@@ -4,98 +4,88 @@ rollup bundles for cjs, esm
 import path from 'path';
 import vue from 'rollup-plugin-vue';
 import commonjs from 'rollup-plugin-commonjs';
-import multiEntry from 'rollup-plugin-multi-entry';
 import alias from 'rollup-plugin-alias';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import postcss from 'rollup-plugin-postcss';
-import atImport from 'postcss-import';
+// import postcss from 'rollup-plugin-postcss';
+// import atImport from 'postcss-import';
 import serve from 'rollup-plugin-serve';
 import replace from 'rollup-plugin-replace';
+import livereload from 'rollup-plugin-livereload';
+import babel from 'rollup-plugin-babel';
 
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
-// const excludePaths = [
-//   'src/components/**/examples/*',
-//   'src/components/**/examples/demo/*',
-//   'src/components/**/comps/*',
-//   'src/components/Utilities/*',
-//   // 'src/components/_index.js',
-//   'src/components/examples.js',
-// ];
+const isDev = process.env.ROLLUP_WATCH;
+console.log('isDev', isDev); /* eslint-disable-line */
 
-export default [
-  // {
-  //   input: {
-  //     include: ['src/components/_index.js'],
-  //     exclude: excludePaths,
-  //   },
-  //   output: [
-  //     {
-  //       file: 'dist/cedar.cjs.js',
-  //       format: 'cjs',
-  //     },
-  //     {
-  //       file: 'dist/cedar.esm.js',
-  //       format: 'esm',
-  //     },
-  //   ],
-  //   plugins: [
-  //     multiEntry(),
-  //     alias({
-  //       resolve: ['.vue', '.json', '.js'],
-  //       srcdir: resolve('src'),
-  //       cssdir: resolve('src/css'),
-  //       assetsdir: resolve('src/assets'),
-  //       componentsdir: resolve('src/components'),
-  //       compositionsdir: resolve('src/compositions'),
-  //       directivesdir: resolve('src/directives'),
-  //       mixinsdir: resolve('src/mixins'),
-  //     }),
-  //     commonjs(),
-  //     vue(),
-  //     // resolve2(),
-  //     // babel({
-  //     //   exclude: 'node_modules/**', // only transpile our source code
-  //     //   runtimeHelpers: true,
-  //     // }),
-  //   ],
-  // },
-  {
-    input: {
-      include: ['src/dev.js'],
-    },
-    output: [
-      {
-        file: 'public/cedar.iife.js',
-        format: 'iife',
-      },
-    ],
-    plugins: [
-      multiEntry(),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('dev'),
-      }),
-      alias({
-        resolve: ['.vue', '.json', '.js'],
-        srcdir: resolve('src'),
-        cssdir: resolve('src/css'),
-        assetsdir: resolve('src/assets'),
-        componentsdir: resolve('src/components'),
-        compositionsdir: resolve('src/compositions'),
-        directivesdir: resolve('src/directives'),
-        mixinsdir: resolve('src/mixins'),
-      }),
-      nodeResolve(),
-      vue(),
-      commonjs(),
-      serve('public'),
-      // resolve2(),
-      // babel({
-      //   exclude: 'node_modules/**', // only transpile our source code
-      //   runtimeHelpers: true,
-      // }),
-    ],
-  },
+const plugins = [
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(isDev),
+  }),
+  alias({
+    resolve: ['.vue', '.json', '.js'],
+    srcdir: resolve('src'),
+    cssdir: resolve('src/css'),
+    assetsdir: resolve('src/assets'),
+    componentsdir: resolve('src/components'),
+    compositionsdir: resolve('src/compositions'),
+    directivesdir: resolve('src/directives'),
+    mixinsdir: resolve('src/mixins'),
+  }),
+  nodeResolve(),
+  vue(),
+  commonjs(),
 ];
+
+const devConfig = {
+  input: 'src/dev.js',
+  output: {
+    file: 'public/kitchen-sink.iife.js',
+    format: 'iife',
+    name: 'cedar',
+  },
+};
+
+const config = {
+  input: 'src/components/_index.js',
+  output: [
+    {
+      file: 'dist/cedar/cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/cedar/esm.js',
+      format: 'esm',
+    },
+  ],
+  plugins,
+};
+
+if (isDev) {
+  Object.assign(config, devConfig);
+
+  config.plugins.push(
+    serve({
+      open: true,
+      contentBase: 'public',
+      port: 3000,
+    }),
+    livereload(),
+  );
+
+  config.watch = {
+    clearScreen: false,
+  };
+} else {
+  config.plugins.push(
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+    }),
+  );
+}
+
+console.log('config', config); /* eslint-disable-line */
+export default config;
