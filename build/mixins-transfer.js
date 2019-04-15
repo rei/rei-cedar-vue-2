a71e816106ac7b7072bae7fcec4c937e101fde67
Cole Willsea <cowills@rei.com>
2019-04-15 12:58:12 -0700

    refactor(variables): fix repo name for variables transfer

--- build/mixins-transfer.js
+++ build/mixins-transfer.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs-extra');
 const glob = require('glob');
 
-const DEST_REPO_NAME = 'rei-cedar-mixins';
+const DEST_REPO_NAME = 'rei-cedar-component-variables';
 const DEST_PATH = 'dist/scss';
 const SUPPORTED_COMPONENTS = [
   'CdrButton.vars.scss',
@@ -22,7 +22,7 @@ files.forEach((f) => {
   const ext = path.extname(f);
   const outFname = fname.replace(ext, '.scss');
   if (!SUPPORTED_COMPONENTS.includes(outFname)) return console.log(`skipping ${outFname}`);
-  
+
   const outDest = `${destMixinsDir}/${outFname}`;
   fs.copySync(f, outDest);
   index += `@import "./${outFname}";\n`;
