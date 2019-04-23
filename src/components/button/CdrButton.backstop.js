bed9f093687f9e2602e5f88893a7c9edbbac848d
Lance Harper <laharpe@rei.com>
2019-04-23 10:50:50 -0700

    refactor(button): restore button to backstop specs

--- src/components/button/CdrButton.backstop.js
+++ src/components/button/CdrButton.backstop.js
@@ -5,19 +5,19 @@ module.exports = [
     selectors: [
       '[data-backstop="buttons"]',
     ],
-    // focusSelectors: [
-    //   '[data-backstop="cdr-button--size"]',
-    //   '[data-backstop="cdr-button--disabled"]',
-    //   '[data-backstop="cdr-button--secondary"]',
-    //   '[data-backstop="cdr-button--anchor"]',
-    //   '[data-backstop="cdr-button--icon"]',
-    // ],
-    // hoverSelectors: [
-    //   '[data-backstop="cdr-button--size"]',
-    //   '[data-backstop="cdr-button--disabled"]',
-    //   '[data-backstop="cdr-button--secondary"]',
-    //   '[data-backstop="cdr-button--anchor"]',
-    //   '[data-backstop="cdr-button--icon"]',
-    // ],
+    focusSelectors: [
+      '[data-backstop="cdr-button--size"]',
+      '[data-backstop="cdr-button--disabled"]',
+      '[data-backstop="cdr-button--secondary"]',
+      '[data-backstop="cdr-button--anchor"]',
+      '[data-backstop="cdr-button--icon"]',
+    ],
+    hoverSelectors: [
+      '[data-backstop="cdr-button--size"]',
+      '[data-backstop="cdr-button--disabled"]',
+      '[data-backstop="cdr-button--secondary"]',
+      '[data-backstop="cdr-button--anchor"]',
+      '[data-backstop="cdr-button--icon"]',
+    ],
   },
 ];
