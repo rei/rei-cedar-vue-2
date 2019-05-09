fc5feb9ec87b8ae328b4fac09d6fc135e06a8633
Darin Trimillos <darin@trimillos.com>
2019-05-09 11:50:29 -0700

    test(jest.config.js): ignore some test paths

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,12 @@ const config = {
     'mixinsdir(.*)$': '<rootDir>/src/mixins/$1',
     'distdir(.*)$': '<rootDir>/dist/$1',
   },
+  modulePathIgnorePatterns: [
+    'rollup.config.test.js',
+    '<rootDir>/test/e2e/',
+    '<rootDir>/templates/__tests__',
+    '<rootDir>/src/components/themer',
+  ],
   transform: {
     '^.+\\.(js|jsx)?$': 'babel-jest',
     '.+\\.(css|styl|less|sass|scss)$': '<rootDir>/node_modules/jest-css-modules-transform',
