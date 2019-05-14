0045313194e885876c07c8b42d6386029c604f53
Darin Trimillos <darin@trimillos.com>
2019-05-14 11:03:30 -0700

    test(runner.js): passing port variable to e2e server

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -5,35 +5,31 @@ const spawn = require('cross-spawn');
 const http = require('http');
 const handler = require('serve-handler');
 
-console.log('runner.js happening');
-
 const server = http.createServer((request, response) => {
   handler(request, response, {
     public: './public'
   });
 });
 
-server.listen(3000, () => {
-  console.log('Running at http://localhost:3000');
+server.listen(process.env.PORT, () => {
+
+  let opts = process.argv.slice(2);
+  if (opts.indexOf('--config') === -1) {
+    opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']);
+  }
+  if (opts.indexOf('--env') === -1) {
+    opts = opts.concat(['--env', 'chrome']);
+  }
+
+  const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' });
+
+  runner.on('exit', (code) => {
+    server.close();
+    process.exit(code);
+  });
+
+  runner.on('error', (err) => {
+    server.close();
+    throw err;
+  });
 });
-
-let opts = process.argv.slice(2);
-if (opts.indexOf('--config') === -1) {
-  opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']);
-}
-if (opts.indexOf('--env') === -1) {
-  opts = opts.concat(['--env', 'chrome']);
-}
-
-const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' });
-
-runner.on('exit', (code) => {
-  server.close();
-  process.exit(code);
-});
-
-runner.on('error', (err) => {
-  server.close();
-  throw err;
-});
-
