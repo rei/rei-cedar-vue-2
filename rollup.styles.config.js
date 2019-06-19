147729b112bc42f6e67b762e5d28880de28dcb88
Darin Trimillos <darin@trimillos.com>
2019-06-19 13:25:25 -0700

    build(reset styles): updated rollup config to build separate reset.css

--- rollup.styles.config.js
+++ rollup.styles.config.js
@@ -17,6 +17,21 @@ const config = [
       }),
     ],
   },
+  {
+    input: 'src/reset.js',
+    output: [
+      {
+        file: 'public/reset.js',
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      postcss({
+        extract: 'dist/reset.css',
+        extensions: ['.scss', '.css'],
+      }),
+    ],
+  },
 ];
 
 export default config;
