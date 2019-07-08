47b00a13c954e4d2196d2a86a7d38f244b69a826
Lance Harper <laharpe@rei.com>
2019-07-08 13:56:03 -0700

    refactor(icon): change icon component to a render function for better slot support

--- src/components/icon/CdrIcon.vue
+++ src/components/icon/CdrIcon.vue
@@ -1,20 +1,3 @@
-<template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    :class="[sizeClass, inheritColorClass, space]"
-    role="presentation"
-  >
-    <!-- @slot any valid svg xml -->
-    <slot />
-    <use
-      v-if="use"
-      :href="`${use}`"
-      :xlink:href="`${use}`"
-    />
-  </svg>
-</template>
-
 <script>
 /**
  * Cedar 2 component for icons
@@ -48,6 +31,49 @@ export default {
       return this.inheritColor ? this.$style['cdr-icon--inherit-color'] : '';
     },
 
+  },
+  render(h) {
+    const getChildrenTextContent = children => children.map(node => (node.children
+      ? getChildrenTextContent(node.children)
+      : node.text)).join('');
+
+    // default svg wrapper options
+    const defaultOpts = {
+      attrs: {
+        xmlns: 'http://www.w3.org/2000/svg',
+        viewBox: '0 0 24 24',
+        role: 'presentation',
+      },
+      class: [this.sizeClass, this.inheritColorClass, this.space],
+    };
+
+    // 'use' with sprite
+    if (this.use) {
+      return h('svg', defaultOpts, [h('use', {
+        attrs: {
+          href: this.use,
+          'xlink:href': this.use,
+        },
+      }, [])]);
+    }
+
+    // slot content is full svg string TODO: preserve svg attrs, etc.?
+    const slotText = getChildrenTextContent(this.$slots.default);
+    if (slotText.startsWith('<svg')) {
+      console.log('here');
+      const startRx = new RegExp('(<svg[^>]*>)', 'i');
+      const endRx = new RegExp('(</svg>)', 'i');
+      const stripped = slotText.replace(startRx, '').replace(endRx, '');
+
+      return h('svg', Object.assign(defaultOpts, {
+        domProps: {
+          innerHTML: stripped,
+        },
+      }), []);
+    }
+
+    // else partial svg string (assumed)
+    return h('svg', defaultOpts, this.$slots.default);
   },
 };
 </script>
