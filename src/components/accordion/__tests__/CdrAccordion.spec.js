ee19beff0399666048e3e3eaee183fc75ede11bd
Darin Trimillos <darin@trimillos.com>
2019-08-14 09:27:28 -0700

    test(accordion): update accordion tests

--- src/components/accordion/__tests__/CdrAccordion.spec.js
+++ src/components/accordion/__tests__/CdrAccordion.spec.js
@@ -10,6 +10,38 @@ const propsData = {
 };
 
 describe('CdrAccordion', () => {
+  describe('accordion data setup', () => {
+    it('sets maxHeight when starting closed', () => {
+      const wrapper = shallowMount(CdrAccordion, {
+        propsData: {
+          id: 'test',
+          label: 'label',
+          opened: false,
+        },
+        slots: {
+          default: 'This is some slot text.'
+        },
+      });
+      
+      expect(wrapper.vm.maxHeight).toBe(0);
+    });
+
+    it('maxHeight is adjusted in created when starting open', () => {
+      const wrapper = shallowMount(CdrAccordion, {
+        propsData: {
+          id: 'test',
+          label: 'label',
+          opened: true,
+        },
+        slots: {
+          default: 'This is some slot text.'
+        },
+      });
+      
+      expect(wrapper.vm.maxHeight).toBe('0px');
+    });
+  });
+
   describe('toggling accordion', () => {
     it('updates emits an event onClick', () => {
       const wrapper = shallowMount(CdrAccordion, {
@@ -26,43 +58,36 @@ describe('CdrAccordion', () => {
       expect(wrapper.emitted('accordion-toggle'));
     });
 
-    it('maxHeight computed prop', () => {
+    it('updates maxHeight on prop update', () => {
       const wrapper = shallowMount(CdrAccordion, {
         propsData: {
           id: 'test',
           label: 'label',
-          opened: false,
+          opened: true,
         },
         slots: {
           default: 'This is some slot text.'
         },
       });
+      
+      wrapper.setProps({ opened: false });
+      expect(wrapper.vm.maxHeight).toBe(0);
+    });    
+  });
 
-      expect(wrapper.vm.maxHeight).toEqual(0);
-
-      wrapper.setProps({ opened: true });
-      /*
-        Vue-test-utils is not getting the correct maxHeight here when run as a
-        test, so we simply test that it doesn't return 0, which is what it
-        should return when opened = false.
-      */
-      expect(wrapper.vm.maxHeight).not.toEqual(0);
+  it('isOpenClass computed prop', () => {
+    const wrapper = shallowMount(CdrAccordion, {
+      propsData: {
+        id: 'test',
+        label: 'label',
+        opened: false,
+      },
+      slots: {
+        default: 'This is some slot text.'
+      },
     });
 
-    it('isOpenClass computed prop', () => {
-      const wrapper = shallowMount(CdrAccordion, {
-        propsData: {
-          id: 'test',
-          label: 'label',
-          opened: false,
-        },
-        slots: {
-          default: 'This is some slot text.'
-        },
-      });
-
-      expect(wrapper.vm.isOpenClass).toEqual('closed');
-    });
+    expect(wrapper.vm.isOpenClass).toEqual('closed');
   });
 
   it('focused style', () => {
