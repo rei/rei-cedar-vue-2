37ea19e69b03a86ddd75fbc671864df26d57e9af
Cole Willsea <cowills@rei.com>
2019-08-20 15:40:18 -0700

    fix(template): update component template

---
+++ templates/[name].jsx
@@ -0,0 +1,15 @@
+import style from './styles/{NAME-FULLNAME}.pcss';
+
+export default {
+  name: '{NAME-FULLNAME}',
+  data() {
+    return {
+      style,
+    };
+  },
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  },
+};
