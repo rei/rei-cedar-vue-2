7295dc97f57bfff6404cb6ce07143ea1c2ca4f2d
michael <mhewson@gmail.com>
2019-08-27 15:03:37 -0700

    fix(text demo): update to remove un used componenet

--- src/components/text/examples/Text.vue
+++ src/components/text/examples/Text.vue
@@ -6,14 +6,12 @@
 </template>
 
 <script>
-import { CdrText } from 'componentsdir/_index';
 import headings from 'componentsdir/text/examples/demo/Headings';
 import paragraphs from 'componentsdir/text/examples/demo/Paragraphs';
 
 export default {
   name: 'TextExamples',
   components: {
-    CdrText,
     headings,
     paragraphs,
   },
