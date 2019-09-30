1bf5a10f30f28e1d4a1c17f097a8114834056ea9
Kristopher Knabel <kknabel@rei.com>
2019-09-30 07:30:46 -0700

    refactor(select): added additional unit test cases for select
    added unit tests to select component

--- src/components/select/__tests__/CdrSelect.spec.js
+++ src/components/select/__tests__/CdrSelect.spec.js
@@ -118,7 +118,7 @@ describe('cdrSelect.vue', () => {
     expect(wrapper.vm.$refs.select.hasAttribute('multiple')).toBe(true);
   });
 
-  it('emits change event with correct value', () => {
+  it('emits input event with correct value', () => {
     const wrapper = shallowMount(CdrSelect, {
       propsData: {
         label: 'test',
@@ -128,10 +128,58 @@ describe('cdrSelect.vue', () => {
     });
     const select = wrapper.find({ ref: 'select'});
     wrapper.setProps({ value: '1' });
-    select.trigger('change');
-    expect(wrapper.emitted().change[0][0]).toBe('1');
+    select.trigger('input');
+    expect(wrapper.emitted().input[0][0]).toBe('1');
   });
 
+  // Deprecated event and should be removed
+  // when the change event is removed.
+  it('emits change event with correct value', () => {
+    const wrapper = shallowMount(CdrSelect, {
+      propsData: {
+        label: 'test',
+        value: '4',
+        options: ['3', '4'],
+      },
+    });
+    const select = wrapper.find({ ref: 'select'});
+    wrapper.setProps({ value: '3' });
+    select.trigger('change');
+    expect(wrapper.emitted().change[0][0]).toBe('3');
+  });
+
+  it('emits input event with correct value for multiple', () => {
+    const wrapper = shallowMount(CdrSelect, {
+      propsData: {
+        label: 'test',
+        multiple: true,
+        value: ['1', '2'],
+        options: [{
+          value: '1',
+          text: 'one',
+        },
+        {
+          value: '2',
+          text: 'two',
+        },
+        {
+          value: '3',
+          text: 'three',
+        }],
+      },
+    });
+    wrapper.setProps({ value: ['1', '3'] });
+    const propValues = wrapper.vm.value;
+    for(let o of wrapper.vm.$refs.select.options) {
+      propValues.indexOf(o.value) >= 0 ? o.selected = true : o.selected = false;
+    }
+    const select = wrapper.find({ ref: 'select'});
+    select.trigger('input');
+    expect(wrapper.emitted().input[0][0]).toEqual(['1', '3']);
+  });
+
+  // Deprecated event and should be removed
+  // when the change event is removed.
   it('emits change event with correct value for multiple', () => {
     const wrapper = shallowMount(CdrSelect, {
       propsData: {
@@ -160,5 +208,43 @@ describe('cdrSelect.vue', () => {
     const select = wrapper.find({ ref: 'select'});
     select.trigger('input');
     expect(wrapper.emitted().change[0][0]).toEqual(['1', '3']);
+  });
+
+  // NOTE - can't use v-model directly here, targeting the `data` prop instead
+  it('updating v-model data updates the select', () => {
+    const wrapper = shallowMount(CdrSelect, {
+      propsData: {
+        label: 'test',
+        value: '4',
+        options: ['3', '4'],
+      },
+    });
+    const select = wrapper.find({ ref: 'select' });
+    wrapper.setProps({value: '3'});
+    expect(select.element.value).toBe('3');
+  });
+
+  it('renders info slot', () => {
+    const wrapper = shallowMount(CdrSelect, {
+      propsData: {
+        label: 'test',
+      },
+      slots: {
+        'info': '🤠',
+      },
+    });
+    expect(wrapper.find('.cdr-select__info-container').text()).toBe('🤠');
+  });
+
+  it('renders helper-text slot', () => {
+    const wrapper = shallowMount(CdrSelect, {
+      propsData: {
+        label: 'test',
+      },
+      slots: {
+        'helper-text': 'very helpful',
+      },
+    });
+    expect(wrapper.find('.cdr-select__helper-text').text()).toBe('very helpful');
   });
 });
