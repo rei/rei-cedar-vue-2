688187d66cda3add69d5fc43053f7881f76a5558
Darin Trimillos <darin@trimillos.com>
2019-10-09 11:20:46 -0700

    feat(modal): initial commit, add modal files

---
+++ src/components/modal/CdrModal.jsx
@@ -0,0 +1,15 @@
+import style from './styles/CdrModal.scss';
+
+export default {
+  name: 'CdrModal',
+  data() {
+    return {
+      style,
+    };
+  },
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  },
+};
---
+++ src/components/modal/examples/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>modal</h2>
+  </div>
+</template>
+
+<script>
+import Components from 'srcdir/index';
+
+export default {
+  name: 'Modal',
+  components: {
+    ...Components,
+  },
+};
+</script>
+
+<style>
+</style>
