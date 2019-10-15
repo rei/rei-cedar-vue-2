8fef2103559cc83dbfad19487fb4c0212f0a1177
Cole Willsea <cowills@rei.com>
2019-10-15 16:06:18 -0700

    fix(icon): update icon build to properly bind props

--- src/components/icon/comps/list-ragged.jsx
+++ src/components/icon/comps/list-ragged.jsx
@@ -8,7 +8,7 @@ export default {
   },
   extends: CdrIcon,
   render() {
-    return (<cdr-icon {...this.$props}>
+    return (<cdr-icon {...{props: this.$props} }>
       {this.$slots.default}
       <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h10a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h14a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
     </cdr-icon>)
--- src/components/icon/comps/star-100.jsx
+++ src/components/icon/comps/star-100.jsx
@@ -8,7 +8,7 @@ export default {
   },
   extends: CdrIcon,
   render() {
-    return (<cdr-icon {...this.$props}>
+    return (<cdr-icon {...{props: this.$props} }>
       {this.$slots.default}
       <path d="M5.686 22.066l1.668-7.552L1.82 9.465l7.298-.659 2.877-7.146 2.877 7.146 7.298.659-5.534 5.049 1.668 7.552-6.309-4.006z"></path>
     </cdr-icon>)
