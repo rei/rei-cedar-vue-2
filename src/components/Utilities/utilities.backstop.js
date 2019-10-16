948825eb221e3cee9f59af83d4211621dc1b8793
Cole Willsea <cowills@rei.com>
2019-10-16 14:14:19 -0700

    fix(test): update broken backstop tests

--- src/components/Utilities/utilities.backstop.js
+++ src/components/Utilities/utilities.backstop.js
@@ -3,13 +3,9 @@ module.exports = [
     url: 'http://localhost:3000/#/utilities',
     label: 'Utility classes',
     selectors: [
-      '[data-backstop="a11y-utilities"]',
       '[data-backstop="visibility-utilities"]',
       '[data-backstop="align-utilities"]',
       '[data-backstop="utility-container"]',
-    ],
-    focusSelectors: [
-      '[data-backstop="a11y-skip-link"] > a',
     ],
   },
 ];
