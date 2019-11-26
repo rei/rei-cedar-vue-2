e70953bb92f4b51a9ba0aec95818df09652de0d5
Cole Willsea <cowills@rei.com>
2019-11-26 14:15:13 -0800

    feat(css): decompose utilities output into multiple files

--- build/extract-css.js
+++ build/extract-css.js
@@ -1,7 +1,7 @@
 const sass = require('node-sass');
 const tildeImporter = require('node-sass-package-importer');
 const fs = require('fs-extra');
-const glob = require("glob");
+const glob = require('glob');
 const chalk = require('chalk');
 const postcss = require('postcss');
 const postcssrc = require('postcss-load-config');
@@ -10,30 +10,69 @@ const packageJson = require('../package.json')
 
 const env = process.env.NODE_ENV;
 
-const globals = ['reset', 'utilities'];
-const components = glob.sync('./src/components/**/styles/*.scss');
+buildCss({
+  srcPath: './src/css/utilities.scss',
+  outPath: './dist/utilities-compiled.css',
+  scopeClasses: false,
+});
 
-const globalOutputs = globals.map((file) => buildCss(file, false));
-const componentOutputs = components.map((file) => buildCss(file, true));
-const outputs = globalOutputs.concat(componentOutputs);
+buildCss({
+  srcPath: './src/css/reset.scss',
+  outPath: './dist/style/reset.css',
+  scopeClasses: false,
+});
 
-const outFile = outputs.map(file => `@import url('${file.replace('.', '@rei/cedar')}');`).join('\n');
+// Create individual utility outputs
+const utilities = glob.sync('./src/css/utility/*.scss')
+  .map((path) => {
+    const name = path.split('utility/_')[1];
+    return {
+      srcPath: path,
+      outPath: `./dist/style/${name.replace('scss', 'css')}`,
+      scopeClasses: false,
+    }
+  }
+);
 
-fs.outputFile('./dist/cdr-full.css', outFile, function(err) {
+utilities.forEach(buildCss);
+// Create utilities index file which imports all of the individual utility outputs
+const utilsFile = utilities.map(createImport).join('\n');
+fs.outputFile('./dist/style/utilities.css', utilsFile, function(err) {
+  if (!err) {
+    console.log(chalk.green(`success! created utilities.css`));
+  }
+});
+
+const components = glob.sync('./src/components/**/styles/*.scss')
+  .map((path) => {
+    const name = path.split('/styles/')[1];
+    return {
+      srcPath: path,
+      outPath: `./dist/style/${name.replace('scss', 'css')}`,
+      scopeClasses: true,
+    }
+  }
+);
+
+components.forEach(buildCss);
+
+const outFile = [{outPath: './dist/style/reset.css'}]
+  .concat(utilities)
+  .concat(components)
+  .map(createImport)
+  .join('\n');
+
+fs.outputFile('./dist/style/cdr-full.css', outFile, function(err) {
   if (!err) {
     console.log(chalk.green(`success! created cdr-full.css`));
   }
 });
 
-function buildCss(file, scopeClasses) {
-  const srcPath = scopeClasses ?
-    file :
-    `./src/css/${file}.scss`;
-    
-  const outPath = scopeClasses ?
-    `./dist/style/${file.split('/')[5].replace('scss', 'css')}` :
-    `./dist/style/${file}.css`;
+function createImport(data) {
+  return `@import url('${data.outPath.replace('.', '@rei/cedar')}');`
+}
 
+function buildCss({ srcPath, outPath, scopeClasses }) {
   sass.render({
     file: srcPath,
     outputStyle: 'compressed',
@@ -48,9 +87,14 @@ function buildCss(file, scopeClasses) {
             generateScopedName: function (name) {
               // scope classes for components
               return `${name}_${packageJson.version}`;
-            }
+            },
+            getJSON: function noop() {}
+            // Passing a noop as getJSON makes it so postcss-modules does not output a JSON
+            // representation of the transformed class names into the source folder.
+            // This JSON data gets handled by the JS build and is not needed here.
           }))
         }
+        options.from = srcPath; // enables source maps, browserlist, etc.
         postcss(plugins)
         .process(result.css, options)
         .then((result) => {
@@ -63,6 +107,6 @@ function buildCss(file, scopeClasses) {
       });
     }
   });
-  
+
   return outPath;
 }
