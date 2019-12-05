2e418beeaa1e3c70348f44c86a3e5b21cc9b264c
Lance Harper <laharpe@rei.com>
2019-12-05 15:02:57 -0800

    refactor(lint): fix lint error

--- src/components/radio/CdrRadio.jsx
+++ src/components/radio/CdrRadio.jsx
@@ -79,7 +79,12 @@ export default {
     return (
       <div class={clsx(this.space, this.style['cdr-radio__wrap'])}>
         <label
-          class={clsx(this.style[this.baseClass], this.modifierClass, this.labelClass, this.sizeClass)}
+          class={clsx(
+            this.style[this.baseClass],
+            this.modifierClass,
+            this.labelClass,
+            this.sizeClass,
+          )}
           ref="label"
         >
           <input
