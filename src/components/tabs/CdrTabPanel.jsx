288639305d8957c9005672af0e4502eed7a11328
Darin Trimillos <darin@trimillos.com>
2020-03-11 13:45:03 -0700

    refactor(tabs): remove transition hooks

--- src/components/tabs/CdrTabPanel.jsx
+++ src/components/tabs/CdrTabPanel.jsx
@@ -43,20 +43,8 @@ export default {
     baseClass() {
       return 'cdr-tab-panel';
     },
-    animationHooks() {
-      return {
-        beforeEnter: this.setEnterStart,
-        afterEnter: this.setEnterEnd,
-        beforeLeave: this.setLeaveStart,
-        afterLeave: this.setLeaveEnd,
-        ...this.$listeners,
-      };
-    },
     activeClass() {
       return this.active ? this.style['cdr-tab-panel-active-fly-right'] : this.style['cdr-tab-panel-inactive-fly-left']; // eslint-disable-line
-    },
-    animationDirectionClass() {
-      return this.animationDirection ? `cdr-tab-panel-${this.animationDirection}` : null;
     },
   },
   methods: {
@@ -71,30 +59,6 @@ export default {
     },
     setOffsetX(x) {
       this.offsetX = x;
-    },
-    setEnterStart(element) {
-      console.log('setEnterStart');
-      const el = element;
-      el.style.animationDirection = 'reverse';
-      el.style.animationTimingFunction = 'cubic-bezier(0.4, 0, 0.68, .06)';
-      el.classList.add(this.style[this.animationDirection]);
-    },
-    setEnterEnd(element) {
-      console.log('setEnterEnd');
-      const el = element;
-      el.style.animationDirection = '';
-      el.classList.remove(this.style[this.animationDirection]);
-    },
-    setLeaveStart(element) {
-      console.log('setLeaveStart');
-      const el = element;
-      el.classList.add(this.style[this.animationDirection]);
-      el.style.animationTimingFunction = 'cubic-bezier(0.32, 0.94, 0.6, 1)';
-    },
-    setLeaveEnd(element) {
-      console.log('setLeaveEnd'); 
-      const el = element;
-      el.classList.remove(this.style[this.animationDirection]);
     },
     handleUpArrowNav() {
       this.$parent.setFocusToActiveTabHeader();
