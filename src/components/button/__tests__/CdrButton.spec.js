4b657762a9f2ecb4ab26d2e041dcf1ce52db9ad1
Cole Willsea <cowills@rei.com>
2020-03-18 10:39:51 -0700

    chore(button): update with-border test

--- src/components/button/__tests__/CdrButton.spec.js
+++ src/components/button/__tests__/CdrButton.spec.js
@@ -62,9 +62,11 @@ describe('CdrButton', () => {
     const wrapper = shallowMount(CdrButton, {
       propsData: {
         iconOnly: true,
+        withBorder: true,
       }
     });
     expect(wrapper.classes()).toContain('cdr-button--icon-only');
+    expect(wrapper.classes()).toContain('cdr-button--with-border');
   });
 
   it('adds icon class when slot is used', () => {
