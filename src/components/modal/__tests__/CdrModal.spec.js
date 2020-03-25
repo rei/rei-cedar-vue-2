e0ed50e61d3ac89a6cd12bef5a9a302e5582fbbd
Darin Trimillos <darin@trimillos.com>
2020-03-25 08:53:52 -0700

    test(modal): update tests

--- src/components/modal/__tests__/CdrModal.spec.js
+++ src/components/modal/__tests__/CdrModal.spec.js
@@ -5,7 +5,7 @@ import Vue from 'vue';
 import CdrButton from 'componentdir/button/CdrButton';
 
 describe('CdrModal.vue', () => {
-  it('renders correctly', () => {
+  it('renders all slots', () => {
     const wrapper = shallowMount(CdrModal, {
       propsData: {
         opened: true,
@@ -19,6 +19,39 @@ describe('CdrModal.vue', () => {
       },
       attachToDocument: true,
     });
+    /*
+      - begins opened
+      - renders all slots for screenshots
+      - close modal
+    */
+
     expect(wrapper.element).toMatchSnapshot();
+    wrapper.setProps({ opened: false });
+    setTimeout(() => {
+      expect(wrapper.vm.reallyClosed).toBe(true);
+    }, 300);
+    // console.log('things', wrapper.vm.totalHeight, wrapper.vm.fullScreen);
+
+  });
+
+  it('leaves optional slots empty', () => {
+    const wrapper = shallowMount(CdrModal, {
+      propsData: {
+        opened: false,
+        label: "Label is the modal title"
+      },
+      slots: {
+        scrollingContentSlot: 'Main content',
+      },
+      attachToDocument: true,
+    });
+
+    wrapper.setProps({ opened: true });
+    Vue.nextTick(() => {
+      expect(wrapper.element).toMatchSnapshot();
+      // setTimeout(() => {
+      //   expect(wrapper.find({ ref: wrapper }).scrollTop).toBe(0);
+      // }, 500);
+    });
   });
 });
