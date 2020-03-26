70ff6115cac0ee601ee88f40580546b7f691c10e
Cole Willsea <cowills@rei.com>
2020-03-26 13:48:18 -0700

    chore(card): add complex card example to dev environment

---
+++ src/components/card/examples/demo/complexCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <cdr-card class="card-example">
+      <section>
+        <div>
+          <cdr-img
+            alt="card test image"
+            src="test-image.png"
+            modifier="responsive"
+          />
+        </div>
+        <div class="cdr-space-inset-one-x">
+          <cdr-text
+            tag="h2"
+            modifier="heading-serif-500"
+          >
+            Complex Card Title
+          </cdr-text>
+          <cdr-rating
+            rating="4.2"
+            count="12"
+            size="small"
+          />
+          <cdr-text modifier="body-300">
+            Card content
+          </cdr-text>
+        </div>
+      </section>
+    </cdr-card>
+  </div>
+</template>
+
+<script>
+import * as Components from 'srcdir/index';
+
+
+export default {
+  name: 'ComplexCard',
+  components: Components,
+};
+</script>
+
+<style>
+.card-example {
+  max-width: 250px;
+  width: 250px;
+}
+</style>
