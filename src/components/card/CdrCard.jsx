f46d25077a088f2b71343b2f083bc670d6159264
Cole Willsea <cowills@rei.com>
2020-05-06 14:06:37 -0700

    feat(card): remove linked prop from card, always use hover effect

--- src/components/card/CdrCard.jsx
+++ src/components/card/CdrCard.jsx
@@ -1,14 +1,7 @@
-import clsx from 'clsx';
 import style from './styles/CdrCard.scss';
 
 export default {
   name: 'CdrCard',
-  props: {
-    linked: {
-      type: Boolean,
-      default: false,
-    },
-  },
   data() {
     return {
       style,
@@ -18,12 +11,9 @@ export default {
     baseClass() {
       return 'cdr-card';
     },
-    linkedClass() {
-      return this.linked ? `${this.baseClass}--linked` : null;
-    },
   },
   render() {
-    return (<article class={clsx(this.style[this.baseClass], this.style[this.linkedClass])}>
+    return (<article class={this.style[this.baseClass]}>
       {this.$slots.default}
     </article>);
   },
