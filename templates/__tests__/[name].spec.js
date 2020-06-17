d48acf379f12d7b56c5eeb46137374bf9b73d456
Cole Willsea <cowills@rei.com>
2020-06-17 16:15:51 -0700

    fix(generator): fix generated test logic, add basic snapshot, basic backstop config

--- templates/__tests__/[name].spec.js
+++ templates/__tests__/[name].spec.js
@@ -1,7 +1,12 @@
-import { shallow } from '@vue/test-utils';
-import {NAME-FULLNAME} from 'distdir/cedar.js';
+import { shallowMount } from '@vue/test-utils';
+import {NAME-FULLNAME} from 'componentdir/{NAME-CAMEL}/{NAME-FULLNAME}';
 
-describe('{NAME-FULLNAME}.vue', () => {
+describe('{NAME-FULLNAME}', () => {
+  it('matches snapshot', () => {
+    const wrapper = shallowMount({NAME-FULLNAME});
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
   it('has a failing test by default so you remember to do them', () => {
     expect(false).toBe(true);
   });
