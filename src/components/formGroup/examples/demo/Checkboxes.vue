6ac7e2f15bcdb6da18ea03e24e860d11c009505d
Cole Willsea <cowills@rei.com>
2020-06-25 11:21:54 -0700

    feat(form-group): add label slot, default text styling, remove border, add spacing

--- src/components/formGroup/examples/demo/Checkboxes.vue
+++ src/components/formGroup/examples/demo/Checkboxes.vue
@@ -14,17 +14,37 @@
         v-model="exGroup"
       >C</cdr-checkbox>
     </cdr-form-group>
+
+    <cdr-form-group>
+
+      <template slot="label">
+        <cdr-text modifier="heading-sans-600">Optional Label Override Example</cdr-text>
+      </template>
+      <cdr-checkbox
+        custom-value="A"
+        v-model="exGroup"
+      >A</cdr-checkbox>
+      <cdr-checkbox
+        custom-value="B"
+        v-model="exGroup"
+      >B</cdr-checkbox>
+      <cdr-checkbox
+        custom-value="C"
+        v-model="exGroup"
+      >C</cdr-checkbox>
+    </cdr-form-group>
   </div>
 </template>
 
 <script>
-import { CdrFormGroup, CdrCheckbox } from 'srcdir/index';
+import { CdrFormGroup, CdrCheckbox, CdrText } from 'srcdir/index';
 
 export default {
   name: 'ParagraphsDemo',
   components: {
     CdrFormGroup,
     CdrCheckbox,
+    CdrText,
   },
   data() {
     return {
