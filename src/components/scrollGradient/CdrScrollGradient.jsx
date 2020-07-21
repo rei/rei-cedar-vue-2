bf2af101601e41b34aa57079aeea162a50bd83c2
Cole Willsea <cowills@rei.com>
2020-07-21 11:30:40 -0700

    fix(scroll-gradient): clean up linter errors, consoles

--- src/components/scrollGradient/CdrScrollGradient.jsx
+++ src/components/scrollGradient/CdrScrollGradient.jsx
@@ -1,8 +1,6 @@
 import clsx from 'clsx';
 import debounce from 'lodash-es/debounce';
-import {
-  CdrColorBackgroundPrimary, CdrSpaceOneX, CdrSpaceHalfX,
-} from '@rei/cdr-tokens/dist/js/cdr-tokens.esm';
+import { CdrColorBackgroundPrimary } from '@rei/cdr-tokens/dist/js/cdr-tokens.esm';
 import style from './styles/CdrScrollGradient.scss';
 import BuildClass from '../../mixins/buildClass';
 
@@ -39,58 +37,6 @@ export default {
       overflowBottom: false,
     };
   },
-  mounted() {
-    this.$nextTick(() => {
-      this.calculateOverflow();
-    });
-    // Check for header overflow on window resize for gradient behavior.
-    window.addEventListener('resize', debounce(() => {
-      this.calculateOverflow();
-    }, 500));
-    // Check for header overflow on widow resize for gradient behavior.
-    this.$refs.cdrScrollGradientContent.addEventListener('scroll', debounce(() => {
-      this.calculateOverflow();
-    }, 50));
-  },
-  methods: {
-    calculateOverflow() {
-      if (this.vertical) {
-        const contentHeight = this.$refs.cdrScrollGradientContent.children[0].offsetHeight;
-        const containerHeight = this.$refs.cdrScrollGradientContainer.offsetHeight;
-        console.log(contentHeight, containerHeight)
-        if (contentHeight > containerHeight) {
-          const scrollY = this.$refs.cdrScrollGradientContent.scrollTop;
-          this.overflowTop = scrollY > 1;
-          this.overflowBottom = (scrollY + 1) < (contentHeight - containerHeight);
-            console.log({scrollY, ot: this.overflowTop, ob: this.overflowBottom, contentHeight, containerHeight})
-        } else {
-          this.overflowTop = false;
-          this.overflowBottom = false;
-        }
-      }
-
-      if (this.horizontal) {
-        const contentWidth = this.$refs.cdrScrollGradientContent.children[0].offsetWidth;
-        const containerWidth = this.$refs.cdrScrollGradientContainer.offsetWidth;
-        console.log(contentWidth, containerWidth)
-        if (contentWidth > containerWidth) {
-          const scrollX = this.$refs.cdrScrollGradientContent.scrollLeft;
-          this.overflowLeft = scrollX > 1;
-          this.overflowRight = (scrollX + 1) < (contentWidth - containerWidth);
-            console.log({scrollX, ot: this.overflowLeft, ob: this.overflowRight, contentWidth, containerWidth})
-        } else {
-          this.overflowLeft = false;
-          this.overflowRight = false;
-        }
-      }
-    },
-    createGradientStyle(direction) {
-      const gradient = `linear-gradient(to ${direction}, rgba(255, 255, 255, 0), ${this.backgroundColor})`;
-      return {
-        background: gradient,
-      };
-    }
-  },
   computed: {
     baseClass() {
       return 'cdr-scroll-gradient';
@@ -121,13 +67,61 @@ export default {
       return this.createGradientStyle('bottom');
     },
   },
+  mounted() {
+    this.$nextTick(() => {
+      this.calculateOverflow();
+    });
+    // Check for header overflow on window resize for gradient behavior.
+    window.addEventListener('resize', debounce(() => {
+      this.calculateOverflow();
+    }, 500));
+    // Check for header overflow on widow resize for gradient behavior.
+    this.$refs.cdrScrollGradientContent.addEventListener('scroll', debounce(() => {
+      this.calculateOverflow();
+    }, 50));
+  },
+  methods: {
+    calculateOverflow() {
+      if (this.vertical) {
+        const contentHeight = this.$refs.cdrScrollGradientContent.children[0].offsetHeight;
+        const containerHeight = this.$refs.cdrScrollGradientContainer.offsetHeight;
+        if (contentHeight > containerHeight) {
+          const scrollY = this.$refs.cdrScrollGradientContent.scrollTop;
+          this.overflowTop = scrollY > 1;
+          this.overflowBottom = (scrollY + 1) < (contentHeight - containerHeight);
+        } else {
+          this.overflowTop = false;
+          this.overflowBottom = false;
+        }
+      }
+
+      if (this.horizontal) {
+        const contentWidth = this.$refs.cdrScrollGradientContent.children[0].offsetWidth;
+        const containerWidth = this.$refs.cdrScrollGradientContainer.offsetWidth;
+        if (contentWidth > containerWidth) {
+          const scrollX = this.$refs.cdrScrollGradientContent.scrollLeft;
+          this.overflowLeft = scrollX > 1;
+          this.overflowRight = (scrollX + 1) < (contentWidth - containerWidth);
+        } else {
+          this.overflowLeft = false;
+          this.overflowRight = false;
+        }
+      }
+    },
+    createGradientStyle(dir) {
+      const bg = `linear-gradient(to ${dir}, rgba(255, 255, 255, 0), ${this.backgroundColor})`;
+      return {
+        background: bg,
+      };
+    },
+  },
   render() {
     return (
-      <div class={this.style[this.baseClass]}  ref="cdrScrollGradientContainer">
+      <div class={this.style[this.baseClass]} ref="cdrScrollGradientContainer">
         <div class={clsx(
           this.style['cdr-scroll-gradient--overlay'],
           this.style['cdr-scroll-gradient--overlay-left'],
-          this.overflowLeft && this.horizontal  ? this.style['cdr-scroll-gradient--overlay-active'] : '',
+          this.overflowLeft ? this.style['cdr-scroll-gradient--overlay-active'] : '',
         )}
           style={this.gradientLeftStyle}
         ></div>
@@ -135,13 +129,14 @@ export default {
         <div class={clsx(
           this.style['cdr-scroll-gradient--overlay'],
           this.style['cdr-scroll-gradient--overlay-top'],
-          this.overflowTop && this.vertical ? this.style['cdr-scroll-gradient--overlay-active'] : '',
+          this.overflowTop ? this.style['cdr-scroll-gradient--overlay-active'] : '',
         )}
           style={this.gradientTopStyle}
         ></div>
 
 
-        <div class={clsx(this.style['cdr-scroll-gradient--content'], this.styleClass)} ref="cdrScrollGradientContent">
+        <div class={clsx(this.style['cdr-scroll-gradient--content'], this.styleClass)}
+          ref="cdrScrollGradientContent">
           { this.$slots.default }
         </div>
 
@@ -149,7 +144,7 @@ export default {
         <div class={clsx(
           this.style['cdr-scroll-gradient--overlay'],
           this.style['cdr-scroll-gradient--overlay-right'],
-          this.overflowRight && this.horizontal ? this.style['cdr-scroll-gradient--overlay-active'] : '',
+          this.overflowRight ? this.style['cdr-scroll-gradient--overlay-active'] : '',
         )}
           style={this.gradientRightStyle}
         ></div>
@@ -158,7 +153,7 @@ export default {
         <div class={clsx(
           this.style['cdr-scroll-gradient--overlay'],
           this.style['cdr-scroll-gradient--overlay-bottom'],
-          this.overflowBottom && this.vertical ? this.style['cdr-scroll-gradient--overlay-active'] : '',
+          this.overflowBottom ? this.style['cdr-scroll-gradient--overlay-active'] : '',
         )}
           style={this.gradientBottomStyle}
         ></div>
--- src/components/scrollGradient/examples/ScrollGradient.vue
+++ src/components/scrollGradient/examples/ScrollGradient.vue
@@ -1,8 +1,13 @@
 <template>
+  <!-- eslint-disable max-len -->
   <div>
     <h2>scrollGradient</h2>
     <h3>vertical text example</h3>
-    <cdr-scroll-gradient :vertical="true" :background-color="backgroundColor" style="width: 250px; height: 250px;">
+    <cdr-scroll-gradient
+      :vertical="true"
+      :background-color="backgroundColor"
+      style="width: 250px; height: 250px;"
+    >
       <!-- NOTE: need to wrap content in a single element. Wrapper element must know its actual height/width. -->
       <div>
         We'll put a happy little bush here. Everything is happy if you choose to make it that way. Everybody's different. Trees are different. Let them all be individuals. Just let this happen. We just let this flow right out of our minds. It just happens - whether or not you worried about it or tried to plan it. We'll put a happy little bush here. Everything is happy if you choose to make it that way. Everybody's different. Trees are different. Let them all be individuals. Just let this happen. We just let this flow right out of our minds. It just happens - whether or not you worried about it or tried to plan it. Trees get lonely too, so we'll give him a little friend. Painting should do one thing. It should put happiness in your heart. This is the way you take out your frustrations. At home you have unlimited time. Maybe we got a few little happy bushes here, just covered with snow. Put your feelings into it, your heart, it's your world.
@@ -11,48 +16,89 @@
     <br><br>
 
     <h3>vertical elements example</h3>
-    <cdr-scroll-gradient :vertical="true" :background-color="backgroundColor" style="width: 100px; height: 250px;">
+    <cdr-scroll-gradient
+      :vertical="true"
+      :background-color="backgroundColor"
+      style="width: 100px; height: 250px;"
+    >
       <div>
-        <div class="square">scrolly</div>
-        <div class="square">time</div>
-        <div class="square">scroll</div>
-        <div class="square">the</div>
-        <div class="square">things</div>
+        <div class="square">
+          scrolly
+        </div>
+        <div class="square">
+          time
+        </div>
+        <div class="square">
+          scroll
+        </div>
+        <div class="square">
+          the
+        </div>
+        <div class="square">
+          things
+        </div>
       </div>
     </cdr-scroll-gradient>
     <br><br>
     <h3>horizontal text example</h3>
-    <cdr-scroll-gradient :horizontal="true" :background-color="backgroundColor" style="width: 250px;">
+    <cdr-scroll-gradient
+      :horizontal="true"
+      :background-color="backgroundColor"
+      style="width: 250px;"
+    >
       <!-- NOTE: need to set explicit width for pure text -->
       <div style="width: 500px;">
         We'll put a happy little bush here. Everything is happy if you choose to make it that way. Everybody's different. Trees are different. Let them all be individuals. Just let this happen. We just let this flow right out of our minds. It just happens - whether or not you worried about it or tried to plan it. We'll put a happy little bush here. Everything is happy if you choose to make it that way. Everybody's different. Trees are different. Let them all be individuals. Just let this happen. We just let this flow right out of our minds. It just happens - whether or not you worried about it or tried to plan it. Trees get lonely too, so we'll give him a little friend. Painting should do one thing. It should put happiness in your heart. This is the way you take out your frustrations. At home you have unlimited time. Maybe we got a few little happy bushes here, just covered with snow. Put your feelings into it, your heart, it's your world.
       </div>
     </cdr-scroll-gradient>
     <br>
-    <cdr-scroll-gradient :horizontal="true" :background-color="backgroundColor" style="width: 250px;">
+    <cdr-scroll-gradient
+      :horizontal="true"
+      :background-color="backgroundColor"
+      style="width: 250px;"
+    >
       <!-- NOTE: max-content for 1 liner -->
       <div style="width: max-content;">
-        Trees get lonely too, so we'll give him a little friend. Painting should do one thing. It should put happiness in your heart. 
+        Trees get lonely too, so we'll give him a little friend. Painting should do one thing. It should put happiness in your heart.
       </div>
     </cdr-scroll-gradient>
     <br><br>
 
     <h3>horizontal elements example</h3>
-    <cdr-scroll-gradient :horizontal="true" :background-color="backgroundColor" style="width: 250px;">
+    <cdr-scroll-gradient
+      :horizontal="true"
+      :background-color="backgroundColor"
+      style="width: 250px;"
+    >
       <!-- NOTE: need to set explicit width or max-content here. float: left means container has width of 0. flex? -->
       <div style="width: max-content;">
-        <div class="square">la</div>
-        <div class="square">la</div>
-        <div class="square">la,</div>
-        <div class="square">la la</div>
-        <div class="square">la la la</div>
+        <div class="square">
+          la
+        </div>
+        <div class="square">
+          la
+        </div>
+        <div class="square">
+          la,
+        </div>
+        <div class="square">
+          la la
+        </div>
+        <div class="square">
+          la la la
+        </div>
       </div>
     </cdr-scroll-gradient>
 
     <br><br>
 
     <h3>multi-dimensional text example</h3>
-    <cdr-scroll-gradient :vertical="true" :horizontal="true" :background-color="backgroundColor" style="width: 250px; height: 250px;">
+    <cdr-scroll-gradient
+      :vertical="true"
+      :horizontal="true"
+      :background-color="backgroundColor"
+      style="width: 250px; height: 250px;"
+    >
       <div style="width: 500px;">
         I'm going to mix up a little color. We’ll use Van Dyke Brown, Permanent Red, and a little bit of Prussian Blue. Put light against light - you have nothing. Put dark against dark - you have nothing. It's the contrast of light and dark that each give the other one meaning. Everyone wants to enjoy the good parts - but you have to build the framework first.We'll put a happy little bush here. Everything is happy if you choose to make it that way. Everybody's different. Trees are different. Let them all be individuals. Just let this happen. We just let this flow right out of our minds. It just happens - whether or not you worried about it or tried to plan it. We'll put a happy little bush here. Everything is happy if you choose to make it that way. Everybody's different. Trees are different. Let them all be individuals. Just let this happen. We just let this flow right out of our minds. It just happens - whether or not you worried about it or tried to plan it. Trees get lonely too, so we'll give him a little friend. Painting should do one thing. It should put happiness in your heart. This is the way you take out your frustrations. At home you have unlimited time. Maybe we got a few little happy bushes here, just covered with snow. Put your feelings into it, your heart, it's your world. I'm going to mix up a little color. We’ll use Van Dyke Brown, Permanent Red, and a little bit of Prussian Blue. Put light against light - you have nothing. Put dark against dark - you have nothing. It's the contrast of light and dark that each give the other one meaning. Everyone wants to enjoy the good parts - but you have to build the framework first.
       </div>
@@ -79,7 +125,7 @@ export default {
   data() {
     return {
       backgroundColor: CdrColorBackgroundPrimary,
-    }
+    };
   },
   // TODO: make a dev-only background mixin for this logic? also exists in tabs.
   watch: {
