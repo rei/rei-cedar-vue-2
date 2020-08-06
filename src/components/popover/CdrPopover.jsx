1cb18d688224c6a5d492b8afeca050ed89430cb8
Cole Willsea <cowills@rei.com>
2020-08-06 10:41:46 -0700

    refactor(popover): refactor placement logic

--- src/components/popover/CdrPopover.jsx
+++ src/components/popover/CdrPopover.jsx
@@ -99,42 +99,41 @@ export default {
       document.addEventListener('click', this.clickHandler);
     },
     calculatePlacement() {
-      this.pos = this.position;
-      this.corner = undefined;
-      this.$nextTick(() => {
-        const rect = this.$refs.popover.getBoundingClientRect();
-        if (this.pos === 'down' && rect.bottom >= window.innerHeight) {
-          this.pos = 'up';
-        } else if (this.pos === 'up' && rect.top <= 0) {
-          this.pos = 'down';
-        } else if (this.pos === 'left' && rect.left <= 0) {
-          this.pos = 'right';
-        } else if (this.pos === 'right' && rect.right >= window.innerWidth) {
-          this.pos = 'left';
-        }
+      const rect = this.$refs.popover.getBoundingClientRect();
+      if (this.pos === 'down' && rect.bottom >= window.innerHeight) {
+        this.pos = 'up';
+      } else if (this.pos === 'up' && rect.top <= 0) {
+        this.pos = 'down';
+      } else if (this.pos === 'left' && rect.left <= 0) {
+        this.pos = 'right';
+      } else if (this.pos === 'right' && rect.right >= window.innerWidth) {
+        this.pos = 'left';
+      }
 
-        const orientation = this.pos === 'down' || this.pos === 'up' ? 'vertical' : 'horizontal';
+      const orientation = this.pos === 'down' || this.pos === 'up' ? 'vertical' : 'horizontal';
 
-        if (orientation === 'vertical' && rect.left <= 0) {
-          this.corner = 'left'
-        } else if (orientation === 'vertical' && rect.right >= window.innerWidth) {
-          this.corner = 'right'
-        } else if (orientation === 'horizontal' && rect.top <= 0) {
-          this.corner = 'top'
-        } else if (orientation === 'horizontal' && rect.bottom >= window.innerHeight) {
-          this.corner = 'bottom'
-        }
-      })
+      if (orientation === 'vertical' && rect.left <= 0) {
+        this.corner = 'left'
+      } else if (orientation === 'vertical' && rect.right >= window.innerWidth) {
+        this.corner = 'right'
+      } else if (orientation === 'horizontal' && rect.top <= 0) {
+        this.corner = 'top'
+      } else if (orientation === 'horizontal' && rect.bottom >= window.innerHeight) {
+        this.corner = 'bottom'
+      }
     },
     handleOpened() {
       const { activeElement } = document;
 
       this.lastActive = activeElement;
-      this.$nextTick(() => {
-        if (this.autoPosition) {
+
+      this.pos = this.position;
+      this.corner = undefined;
+      if (this.autoPosition) {
+        this.$nextTick(() => {
           this.calculatePlacement();
-        }
-      })
+        });
+      }
 
       setTimeout(() => {
         this.addHandlers();
