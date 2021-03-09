4142a56f6b53b7036cfd63b9ec201bf1e2f96138
Cole Willsea <cowills@rei.com>
2021-03-09 08:33:57 -0800

    fix(form): fix SSR inheritAtttrs bug on radio/checkbox

--- src/components/labelWrapper/CdrLabelWrapper.jsx
+++ src/components/labelWrapper/CdrLabelWrapper.jsx
@@ -7,6 +7,7 @@ import propValidator from '../../utils/propValidator';
 export default {
   name: 'CdrLabelWrapper',
   mixins: [modifier, size],
+  inheritAttrs: false,
   props: {
     labelClass: String,
     contentClass: String,
