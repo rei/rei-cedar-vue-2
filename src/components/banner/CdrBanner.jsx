7efdbec502c70230d8ae28b9a14294ec95794718
Ben Griffin <bgriffi@rei.com>
2021-08-26 14:24:59 -0700

    WIP refactoring cdr-banner for new messaging patterns

--- src/components/banner/CdrBanner.jsx
+++ src/components/banner/CdrBanner.jsx
@@ -25,14 +25,56 @@ export default {
     baseClass() {
       return 'cdr-banner';
     },
-    iconClass() {
+    borderClass() {
       return this.modifyClassName(this.baseClass, this.type);
+    },
+    iconClass() {
+      return this.modifyClassName(`${this.baseClass}__icon-left`, this.type);
+    },
+    mainTheme() {
+      return this.modifyClassName(`${this.baseClass}__main`, this.type);
+    },
+    prominenceClass() {
+      return this.$slots['message-body']
+        ? this.modifyClassName(`${this.baseClass}__wrapper`, 'prominence')
+        : undefined;
+    },
+    noMsgBodyClass() {
+      return !this.$slots['message-body']
+        ? this.modifyClassName(`${this.baseClass}__main`, 'no-msg')
+        : undefined;
     },
   },
   render() {
     return (
-      <div class={clsx(this.style[this.baseClass], this.iconClass)}>
-        {this.$slots.default}
+      <div class={clsx(this.style[this.baseClass], this.borderClass)}>
+        <div class={clsx(this.style['cdr-banner__wrapper'], this.prominenceClass)}>
+          <div class={clsx(this.style['cdr-banner__main'], this.mainTheme, this.noMsgBodyClass)}>
+            {this.$slots['icon-left'] && (
+              <div class={clsx(this.style['cdr-banner__icon-left'], this.iconClass)}>
+                {this.$slots['icon-left']}
+              </div>
+            )}
+            <span class={this.style['cdr-banner__message']}>
+              { this.$slots.default }
+            </span>
+            {this.$slots['icon-right'] && (
+              <div class={this.style['cdr-banner__icon-right']}>
+                {this.$slots['icon-right']}
+              </div>
+            )}
+          </div>
+          {this.$slots['message-body'] && (
+            <div class={this.style['cdr-banner__message-body']}>
+              {this.$slots['message-body']}
+            </div>
+          )}
+        </div>
+        {this.$slots['info-action'] && (
+          <div class={this.style['cdr-banner__info-action']}>
+            {this.$slots['info-action']}
+          </div>
+        )}
       </div>
     );
   },
--- src/components/banner/examples/Banner.vue
+++ src/components/banner/examples/Banner.vue
@@ -2,26 +2,137 @@
   <div>
     <h2>banner</h2>
     <cdr-banner type="info">
-      <icon-information-fill /> info banner
-    </cdr-banner>
-    <br>
-    <cdr-banner type="success">
-      <icon-check-fill /> success banner
+      <template #icon-left>
+        <icon-information-fill inherit-color />
+      </template>
+      Information banner
     </cdr-banner>
     <br>
     <cdr-banner type="warning">
-      <icon-warning-fill /> warning banner
+      <template #icon-left>
+        <icon-warning-fill inherit-color />
+      </template>
+      Warning banner
     </cdr-banner>
     <br>
     <cdr-banner type="error">
-      <icon-x-fill /> error banner
+      <template #icon-left>
+        <icon-error-fill inherit-color />
+      </template>
+      Error banner
+    </cdr-banner>
+    <br>
+    <cdr-banner type="success">
+      <template #icon-left>
+        <icon-check-fill inherit-color />
+      </template>
+      Success banner
     </cdr-banner>
     <br>
     <cdr-banner type="error">
-      <cdr-text class="cdr-text-dev--heading-serif-strong-1200">
-        <icon-x-fill /> error banner
-      </cdr-text>
+      <template #icon-left>
+        <icon-atv />
+      </template>
+      This is the main messaging content
     </cdr-banner>
+
+    <br>
+
+    <cdr-banner type="warning">
+      <template #icon-left>
+        <icon-atv />
+      </template>
+      <strong>Warning!</strong> watch for reckless ATV drivers
+      <template #message-body>
+        They ATV's go vrooom vrooom!
+      </template>
+    </cdr-banner>
+    <br>
+    <cdr-banner type="error">
+      <strong>Oops:</strong> something went wrong
+      <template #message-body>
+        It was probably your fault.
+      </template>
+    </cdr-banner>
+    <br>
+    <cdr-banner type="info">
+      <template #icon-left>
+        <icon-warning-stroke />
+      </template>
+      information banner information banner information banner
+      information banner information banner information banner information banner information banner
+      <template #icon-right>
+        <cdr-button
+          :icon-only="true"
+          aria-label="More information about icon"
+        >
+          <template #icon>
+            <icon-x-lg inherit-color />
+          </template>
+        </cdr-button>
+      </template>
+      <template #info-action>
+        <cdr-button
+          :icon-only="true"
+          aria-label="More information about icon"
+        >
+          <template #icon>
+            <icon-information-stroke inherit-color />
+          </template>
+        </cdr-button>
+      </template>
+      <template #message-body>
+        This is some additional messaging about the status.
+      </template>
+    </cdr-banner>
+    <br>
+    <cdr-banner type="success">
+      <template #icon-left>
+        <icon-check-fill />
+      </template>
+      success banner
+      <template #icon-right>
+        <cdr-button
+          :icon-only="true"
+          aria-label="More information about icon"
+        >
+          <template #icon>
+            <icon-x-lg inherit-color />
+          </template>
+        </cdr-button>
+      </template>
+    </cdr-banner>
+    <br>
+    <cdr-banner type="warning">
+      <template #icon-left>
+        <icon-warning-stroke />
+      </template>
+      warning banner
+      <template #icon-right>
+        <cdr-button
+          :icon-only="true"
+          aria-label="More information about icon"
+        >
+          <template #icon>
+            <icon-brand-rei-ice-axes inherit-color />
+          </template>
+        </cdr-button>
+      </template>
+      <template #info-action>
+        <cdr-button
+          :icon-only="true"
+          aria-label="More information about icon"
+        >
+          <template #icon>
+            <icon-information-stroke inherit-color />
+          </template>
+        </cdr-button>
+      </template>
+      <template #message-body>
+        This is some additional messaging about the status.
+      </template>
+    </cdr-banner>
+    <br>
   </div>
 </template>
 
