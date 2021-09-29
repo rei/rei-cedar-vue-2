69c0042fdca98919e9616c1daae39ac86859a8d6
Ben Griffin <bgriffi@rei.com>
2021-09-29 11:56:07 -0700

    feat(icon): adds new error icon comps

--- src/components/icon/comps/error-stroke.jsx
+++ src/components/icon/comps/error-stroke.jsx
@@ -8,7 +8,7 @@ export default {
   render() {
     return (<cdr-icon {...{props: this.$props, attrs: this.$attrs} }>
       {this.$slots.default}
-      <path role="presentation" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm0 2a8 8 0 100 16 8 8 0 000-16zm0 11.5a1.25 1.25 0 110 2.5 1.25 1.25 0 010-2.5zm.22-9.5a1 1 0 011 1.03l-.191 6.5a1 1 0 01-1 .97h-.058a1 1 0 01-1-.97l-.19-6.5A1 1 0 0111.78 6h.44z"></path>
+      <g><path role="presentation" d="M15.293 16.707a1 1 0 001.414-1.414L9.121 7.707a1 1 0 10-1.414 1.414l7.586 7.586z"></path><path fill-rule="evenodd" d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2s10 4.477 10 10zm-2 0a8 8 0 11-16 0 8 8 0 0116 0z" clip-rule="evenodd"></path></g>
     </cdr-icon>)
   },
 };
